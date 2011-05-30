9b56a698bde597bfabaaaee368ad95ea68cdfa39
Mathias Kaerlev <matpow2@gmail.com>
2011-05-30 21:15:30 +0200

    Fixed py2exe stuff

--- py2exe/build.py
+++ py2exe/build.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
 import py2exe
 
+# just so we always get commands.py first, not the stdlib commands.py
+import sys
+sys.path.insert(0, '../feature_server')
+
 setup(
     console = ['../feature_server/run.py'],
     options = {
