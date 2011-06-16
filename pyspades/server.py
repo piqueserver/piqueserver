b52661b4ac2086b5a032fcc2f40649ef63e2dfa8
Mathias Kaerlev <matpow2@gmail.com>
2011-06-16 22:32:44 +0200

    Oops!

--- pyspades/server.py
+++ pyspades/server.py
@@ -350,8 +350,8 @@ class ServerConnection(BaseConnection):
         These values may need further tuning.
         
         XXX: currently, this is broken.
-        
         """
+        self.position.set(contained.x, contained.y, contained.z)
         position_data.player_id = self.player_id
         position_data.x = contained.x
         position_data.y = contained.y
