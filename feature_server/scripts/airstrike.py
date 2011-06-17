2ac78d15e67b330c67740ef6cb8971ab979903db
Mathias Kaerlev <matpow2@gmail.com>
2011-06-17 21:14:39 +0200

    Forgot scripts

---
+++ feature_server/scripts/airstrike.py
@@ -0,0 +1,150 @@
+from pyspades.server import (ServerProtocol, ServerConnection,
+                            position_data, orientation_data, grenade_packet,
+                            block_action, set_color, intel_action)
+from pyspades.serverloaders import PositionData
+from pyspades.common import coordinates
+from pyspades.collision import distance_3d_vector
+import commands
+
+def airstrike(connection, value = None):
+    if not connection.protocol.airstrikes:
+        return
+    return connection.start_airstrike(value)
+
+commands.add(airstrike)
+
+def apply_script(protocol, connection, config):
+    class AirstrikeConnection(connection):
+        aux = None
+        airstrike = False
+        def desync_grenade(self, x, y, z, orientation_x, fuse):
+            """Gives the appearance of a grenade appearing from thin air by moving
+            an auxiliary player to the target location and then back"""
+            new_position = PositionData()
+            new_position.set((x, y, z), self.player_id)
+            orientation_data.set((orientation_x, 0.0, 0.0), self.player_id)
+            grenade_packet.value = fuse
+            grenade_packet.player_id = self.player_id
+            old_position = PositionData()
+            old_position.set(self.get_location(), self.player_id)
+            packets = [new_position, orientation_data, grenade_packet, old_position]
+            if self.aux is None:
+                self.aux = self.find_aux_connection()
+            if self.aux is not self:
+                for packet in packets:
+                    self.protocol.send_contained(packet, sender = self)
+                old_position.set(self.aux.get_location(), self.aux.player_id)
+                for packet in packets:
+                    packet.player_id = self.aux.player_id
+                    if packet is orientation_data:
+                        target.send_contained(packet, 
+                            target.get_orientation_sequence())
+                    else:
+                        target.send_contained(packet)
+            else:
+                for packet in packets:
+                    self.protocol.send_contained(packet)
+        
+        def find_aux_connection(self):
+            """Attempts to find an allied player far away, preferrably dead,
+            so that we don't see it flipping out right in front of us"""
+            best = None
+            best_distance = 0.0
+            for player in self.team.get_players():
+                if player is self:
+                    continue
+                distance = distance_3d_vector(self.position, player.position)
+                if best is None or player.hp <= 0 and best.hp > 0:
+                    best, best_distance = player, distance
+                    continue
+                if player.hp > 0 and best.hp <= 0:
+                    continue
+                if distance > best_distance:
+                    best, best_distance = player, distance
+            if best is None:
+                best = self
+            return best
+        
+        # airstrike
+        
+        def start_airstrike(self, value = None):
+            score_req = self.protocol.airstrike_min_score_req
+            streak_req = self.protocol.airstrike_streak_req
+            interval = self.protocol.airstrike_interval
+            if value is None and (self.god or self.airstrike):
+                return 'Airstrike support ready! Use with e.g. /airstrike A1'
+            if not self.god:
+                if self.kills < score_req:
+                    return ('You need a total score of %s (kills and intel) to '
+                            'unlock airstrikes!' % score_req)
+                elif not self.airstrike:
+                    kills_left = streak_req - (self.streak % streak_req)
+                    return ('Every %s consecutive kills (without dying) you get an '
+                            'airstrike. %s kills to go!' % (streak_req, kills_left))
+            try:
+                x, y = coordinates(value)
+            except (ValueError):
+                return ("Bad coordinates: should be like 'A4', 'G5'. Look them up "
+                        "in the map.")
+            if (hasattr(self.team, 'last_airstrike') and 
+                reactor.seconds() - self.team.last_airstrike < interval):
+                remain = interval - (reactor.seconds() - self.team.last_airstrike)
+                return ('You must wait %s seconds before your team can launch '
+                        'another airstrike.' % int(remain))
+            self.team.last_airstrike = reactor.seconds()
+            
+            self.airstrike = False
+            self.protocol.send_chat('Ally %s called in an airstrike on '
+                'location %s' % (self.name, value.upper()), global_message = False,
+                team = self.team)
+            self.protocol.send_chat('[WARNING] Enemy air support heading to %s!' %
+                value.upper(), global_message = False, team = self.team.other)
+            reactor.callLater(2.5, self.do_airstrike, x, y)
+        
+        def do_airstrike(self, start_x, start_y):
+            z = 1
+            self.aux = self.find_aux_connection()
+            orientation_x = [1.0, -1.0][self.team.id]
+            start_x = max(0, min(512, start_x + [-64, 64][self.team.id]))
+            range_x = [61, -61][self.team.id]
+            increment_x = [5, -5][self.team.id]
+            del self.team.last_airstrike
+            for round in xrange(12):
+                x = start_x + random.randrange(64)
+                y = start_y + random.randrange(64)
+                fuse = self.protocol.map.get_height(x + range_x, y) * 0.033
+                for i in xrange(5):
+                    x += increment_x
+                    time = round * 0.9 + i * 0.14
+                    reactor.callLater(time, self.desync_grenade, x, y, z,
+                        orientation_x, fuse)
+
+    def add_score(self, score):
+        connection.add_score(self, score)
+        score_req = self.protocol.airstrike_min_score_req
+        streak_req = self.protocol.airstrike_streak_req
+        score_met = (self.kills >= score_req)
+        streak_met = (self.streak >= streak_req)
+        give_strike = False
+        if not score_met:
+            return
+        if self.kills - score < score_req:
+            self.send_chat('You have unlocked airstrike support!')
+            self.send_chat('Each %s-kill streak will clear you for one '
+                           'airstrike.' % streak_req)
+            if streak_met:
+                give_strike = True
+        if not streak_met:
+            return
+        if (self.streak % streak_req == 0 or give_strike):
+            self.send_chat('Airstrike support ready! Launch with e.g. '
+                           '/airstrike B4')
+            self.airstrike = True
+            self.refill()
+    
+    class AirstrikeProtocol(protocol):
+        airstrike_min_score_req = 15
+        airstrike_streak_req = 6
+        airstrike_interval = 20
+    
+    return protocol, AirstrikeConnection
---
+++ feature_server/scripts/rollback.py
@@ -0,0 +1,156 @@
+from map import Map
+from commands import add, admin
+from pyspades.common import coordinates
+
+@admin
+def rollmap(connection, filename = None, first_arg = None, second_arg = None):
+    start_x, start_y, end_x, end_y, z_offset = 0, 0, 512, 512, 0
+    if first_arg is not None and second_arg is None:
+        try:
+            start_x, start_y = coordinates(first_arg)
+            end_x, end_y = start_x + 64, start_y + 64
+        except (ValueError):
+            z_offset = int(first_arg)
+    elif first_arg is not None and second_arg is not None:
+        start_x, start_y = coordinates(first_arg)
+        end_x, end_y = start_x + 64, start_y + 64
+        z_offset = int(second_arg)
+    return connection.protocol.start_rollback(connection, filename,
+        start_x, start_y, end_x, end_y, z_offset)
+
+@admin
+def rollback(connection, value = None):
+    return rollmap(connection, first_arg = value)
+
+@admin
+def rollbackcancel(connection):
+    return connection.protocol.cancel_rollback(connection)
+
+for func in (rollmap, rollback, rollbackcancel):
+    add(func)
+
+def apply_script(protocol, connection, config):
+    rollback_on_game_end = config.get('rollback_on_game_end', False)
+    rollback_map = Map(config['map']).data
+    class RollbackConnection(connection):
+        def on_block_color(self, (r, g, b)):
+            if (self.protocol.rollback_in_progress and
+                self.protocol.rollbacking_player is self):
+                return False
+            
+    class RollbackProtocol(protocol):
+        rollback_in_progress = False
+        rollback_max_rows = 10 # per 'cycle', intended to cap cpu usage
+        rollback_max_packets = 180 # per 'cycle' cap for (unique packets * players)
+        rollback_max_unique_packets = 12 # per 'cycle', each block op is at least 1
+        rollback_time_between_cycles = 0.06
+        rollback_time_between_progress_updates = 10.0
+        rollbacking_player = None
+        rollback_start_time = None
+        rollback_last_chat = None
+        rollback_rows = None
+        rollback_total_rows = None
+        
+        # rollback
+        
+        def start_rollback(self, connection, filename,
+                           start_x, start_y, end_x, end_y, z_offset):
+            if self.rollback_in_progress:
+                return 'Rollback in progress.'
+            map = rollback_map if filename is None else Map(filename).data
+            self.send_chat('%s commenced a rollback...' %
+                (connection.name if connection is not None else 'Map'), irc = True)
+            if connection is None:
+                for player in self.players.values():
+                    connection = player
+                    if player.admin:
+                        break
+            packet_generator = self.create_rollback_generator(connection,
+                self.map, map, start_x, start_y, end_x, end_y, z_offset)
+            self.rollbacking_player = connection
+            self.rollback_in_progress = True
+            self.rollback_start_time = time.time()
+            self.rollback_last_chat = self.rollback_start_time
+            self.rollback_rows = 0
+            self.rollback_total_rows = end_x - start_x
+            self.rollback_cycle(packet_generator)
+        
+        def cancel_rollback(self, connection):
+            if not self.rollback_in_progress:
+                return 'No rollback in progress.'
+            self.end_rollback('Cancelled by %s' % connection.name)
+        
+        def end_rollback(self, result):
+            self.rollback_in_progress = False
+            self.update_entities()
+            self.send_chat('Rollback ended. %s' % result, irc = True)
+        
+        def rollback_cycle(self, packet_generator):
+            if not self.rollback_in_progress:
+                return
+            try:
+                sent = rows = 0
+                while (True):
+                    if rows > self.rollback_max_rows:
+                        break
+                    if sent > self.rollback_max_unique_packets:
+                        break
+                    if sent * len(self.connections) > self.rollback_max_packets:
+                        break
+                    
+                    sent_packets = packet_generator.next()
+                    sent += sent_packets
+                    rows += (sent_packets == 0)
+                self.rollback_rows += rows
+                if (time.time() - self.rollback_last_chat >
+                    self.rollback_time_between_progress_updates):
+                    self.rollback_last_chat = time.time()
+                    progress = (float(self.rollback_rows) /
+                        self.rollback_total_rows * 100.0)
+                    self.send_chat('Rollback progress %s%%' % int(progress))
+            except (StopIteration):
+                self.end_rollback('Time taken: %.2fs' % 
+                    float(time.time() - self.rollback_start_time))
+                return
+            reactor.callLater(self.rollback_time_between_cycles,
+                self.rollback_cycle, packet_generator)
+        
+        def create_rollback_generator(self, connection, mapdata, mapdata_new,
+                                      start_x, start_y, end_x, end_y, z_offset):
+            last_color = None
+            for x in xrange(start_x, end_x):
+                for y in xrange(start_y, end_y):
+                    for z in xrange(63):
+                        packets_sent = 0
+                        block_action.value = None
+                        old_solid = mapdata.get_solid(x, y, z)
+                        new_solid = mapdata_new.get_solid(x, y, z + z_offset)
+                        if old_solid and not new_solid:
+                            block_action.value = DESTROY_BLOCK
+                            mapdata.remove_point_unsafe(x, y, z)
+                        elif not old_solid and new_solid:
+                            block_action.value = BUILD_BLOCK
+                            new_color = mapdata_new.get_color(x, y, z + z_offset)
+                            set_color.value = new_color & 0xFFFFFF
+                            set_color.player_id = connection.player_id
+                            if new_color != last_color:
+                                last_color = new_color
+                                self.send_contained(set_color, save = True)
+                                packets_sent += 1
+                            mapdata.set_point_unsafe_int(x, y, z, new_color)
+                        
+                        if block_action.value is not None:
+                            block_action.x = x
+                            block_action.y = y
+                            block_action.z = z
+                            block_action.player_id = connection.player_id
+                            self.send_contained(block_action, save = True)
+                            packets_sent += 1
+                            yield packets_sent
+                yield 0
+        
+        def on_game_end(self, player):
+            if rollback_on_game_end:
+                self.start_rollback(None, None, 0, 0, 512, 512, 0)
+        
+    return RollbackProtocol, RollbackConnection
