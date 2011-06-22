be7b277380488cc546a465c656448dca4c030abc
duckslingers <duckslingers@gmail.com>
2011-06-22 02:54:45 -0300

    Reworked rollback, dropped z offset parameter

--- feature_server/scripts/rollback.py
+++ feature_server/scripts/rollback.py
@@ -1,33 +1,24 @@
 from map import Map
 from commands import add, admin
 from pyspades.common import coordinates
-from pyspades.server import block_action, set_color
+from pyspades.serverloaders import BlockAction, SetColor
 from twisted.internet import reactor
 from pyspades.constants import *
 import time
+import operator
 
 @admin
-def rollmap(connection, filename = None, first_arg = None, second_arg = None):
-    start_x, start_y, end_x, end_y, z_offset = 0, 0, 512, 512, 0
-    if first_arg is not None and second_arg is None:
-        try:
-            start_x, start_y = coordinates(first_arg)
-            end_x, end_y = start_x + 64, start_y + 64
-        except (ValueError):
-            z_offset = int(first_arg)
-    elif first_arg is not None and second_arg is not None:
-        start_x, start_y = coordinates(first_arg)
+def rollmap(connection, filename = None, value = None):
+    start_x, start_y, end_x, end_y = 0, 0, 512, 512
+    if value is not None:
+        start_x, start_y = coordinates(value)
         end_x, end_y = start_x + 64, start_y + 64
-        z_offset = int(second_arg)
-    rollback_player = connection
-    if connection not in connection.protocol.players:
-        rollback_player = None
-    return connection.protocol.start_rollback(rollback_player, filename,
-        start_x, start_y, end_x, end_y, z_offset)
+    return connection.protocol.start_rollback(connection, filename,
+        start_x, start_y, end_x, end_y)
 
 @admin
-def rollback(connection, first_arg = None, second_arg = None):
-    return rollmap(connection, first_arg = first_arg, second_arg = second_arg)
+def rollback(connection, value = None):
+    return rollmap(connection, value = value)
 
 @admin
 def rollbackcancel(connection):
@@ -66,13 +57,17 @@ def apply_script(protocol, connection, config):
             if self.rollback_in_progress:
                 return 'Rollback in progress.'
             map = rollback_map if filename is None else Map(filename).data
-            self.send_chat('%s commenced a rollback...' %
-                (connection.name if connection is not None else 'Map'), irc = True)
-            if connection is None:
+            message = ('%s commenced a rollback...' %
+                (connection.name if connection is not None else 'Map'))
+            if connection is None or connection not in self.players:
                 for player in self.players.values():
                     connection = player
                     if player.admin:
                         break
+            if connection is None:
+                return ('There must be at least one player in the server '
+                    'to perform a rollback')
+            self.send_chat(message, irc = True)
             packet_generator = self.create_rollback_generator(connection,
                 self.map, map, start_x, start_y, end_x, end_y, z_offset)
             self.rollbacking_player = connection
@@ -97,25 +92,29 @@ def apply_script(protocol, connection, config):
             if not self.rollback_in_progress:
                 return
             try:
-                sent = rows = 0
+                sent_unique = sent_total = rows = 0
                 while (True):
                     if rows > self.rollback_max_rows:
                         break
-                    if sent > self.rollback_max_unique_packets:
+                    if sent_unique > self.rollback_max_unique_packets:
                         break
-                    if sent * len(self.connections) > self.rollback_max_packets:
+                    if sent_total > self.rollback_max_packets:
                         break
                     
-                    sent_packets = packet_generator.next()
-                    sent += sent_packets
-                    rows += (sent_packets == 0)
+                    sent = packet_generator.next()
+                    sent_unique += sent
+                    sent_total += sent * len(self.connections)
+                    rows += (sent == 0)
                 self.rollback_rows += rows
                 if (time.time() - self.rollback_last_chat >
                     self.rollback_time_between_progress_updates):
                     self.rollback_last_chat = time.time()
-                    progress = (float(self.rollback_rows) /
+                    progress = int(float(self.rollback_rows) /
                         self.rollback_total_rows * 100.0)
-                    self.send_chat('Rollback progress %s%%' % int(progress))
+                    if progress < 100:
+                        self.send_chat('Rollback progress %s%%' % progress)
+                    else
+                        self.send_chat('Rollback doing color pass...')
             except (StopIteration):
                 self.end_rollback('Time taken: %.2fs' % 
                     float(time.time() - self.rollback_start_time))
@@ -123,39 +122,66 @@ def apply_script(protocol, connection, config):
             reactor.callLater(self.rollback_time_between_cycles,
                 self.rollback_cycle, packet_generator)
         
-        def create_rollback_generator(self, connection, mapdata, mapdata_new,
-                                      start_x, start_y, end_x, end_y, z_offset):
-            last_color = None
+        def create_rollback_generator(self, connection, old, new,
+                                      start_x, start_y, end_x, end_y):
+            surface = {}
+            block_action = BlockAction()
+            block_action.player_id = connection.player_id
+            set_color = SetColor()
+            set_color.value = 0x000000
+            set_color.player_id = connection.player_id
+            self.send_contained(set_color, save = True)
+            
             for x in xrange(start_x, end_x):
+                block_action.x = x
                 for y in xrange(start_y, end_y):
-                    for z in xrange(63):
-                        packets_sent = 0
+                    block_action.y = y
+                    for z in xrange(64):
+                        block_action.z = z
                         block_action.value = None
-                        old_solid = mapdata.get_solid(x, y, z)
-                        new_solid = mapdata_new.get_solid(x, y, z + z_offset)
+                        old_solid = old.get_solid(x, y, z)
+                        new_solid = new.get_solid(x, y, z)
                         if old_solid and not new_solid:
                             block_action.value = DESTROY_BLOCK
-                            mapdata.remove_point_unsafe(x, y, z)
-                        elif not old_solid and new_solid:
-                            block_action.value = BUILD_BLOCK
-                            new_color = mapdata_new.get_color(x, y, z + z_offset)
-                            set_color.value = new_color & 0xFFFFFF
-                            set_color.player_id = connection.player_id
-                            if new_color != last_color:
-                                last_color = new_color
-                                self.send_contained(set_color, save = True)
-                                packets_sent += 1
-                            mapdata.set_point_unsafe_int(x, y, z, new_color)
-                        
+                            old.remove_point_unsafe(x, y, z)
+                        elif new_solid:
+                            new_color = new.get_color(x, y, z)
+                            new_is_surface = (new_color != 0)
+                            if not old_solid and new_is_surface:
+                                surface[(x, y, z)] = new_color
+                            elif not old_solid and not new_is_surface:
+                                block_action.value = BUILD_BLOCK
+                                old.set_point_unsafe_int(x, y, z, 0)
+                            elif old_solid and new_is_surface:
+                                old_color = old.get_color(x, y, z)
+                                if old_color != new_color:
+                                    surface[(x, y, z)] = new_color
+                                    block_action.value = DESTROY_BLOCK
+                                    old.remove_point_unsafe(x, y, z)
                         if block_action.value is not None:
-                            block_action.x = x
-                            block_action.y = y
-                            block_action.z = z
-                            block_action.player_id = connection.player_id
                             self.send_contained(block_action, save = True)
-                            packets_sent += 1
-                            yield packets_sent
+                            yield 1
                 yield 0
+            last_color = None
+            block_action.value = BUILD_BLOCK
+            for pos, color in sorted(surface.iteritems(),
+                key = operator.itemgetter(1)):
+                packets_sent = 0
+                if color != last_color:
+                    set_color.value = color & 0xFFFFFF
+                    self.send_contained(set_color, sender = connection,
+                        save = True)
+                    packets_sent += 1
+                    last_color = color
+                connection.send_contained(set_color)
+                x, y, z = pos
+                old.set_point_unsafe_int(x, y, z, color)
+                block_action.x = x
+                block_action.y = y
+                block_action.z = z
+                self.send_contained(block_action, save = True)
+                packets_sent += 1
+                yield packets_sent
         
         def on_game_end(self, player):
             if rollback_on_game_end:
