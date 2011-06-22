43867e8e6805b5170768c20d7ba9fe6a378be526
duckslingers <duckslingers@gmail.com>
2011-06-22 03:02:33 -0300

    Autohelp now waits a second then sends the message to everyone

--- feature_server/scripts/autohelp.py
+++ feature_server/scripts/autohelp.py
@@ -1,3 +1,4 @@
+from twisted.internet import reactor
 import re
 
 deuce_name_pattern = re.compile("Deuce\d?\d?$")
@@ -8,11 +9,14 @@ def deuce_howto_match(player, msg):
         not chat_pattern.match(msg) is None)
 
 def apply_script(protocol, connection, config):
+    def send_help(connection):
+        connection.protocol.send_chat("TO CHANGE YOUR NAME: Start Menu-> "
+            "All Programs-> Ace of Spades-> Configuration")
+        connection.protocol.irc_say("* Sent help to %s" % connection.name)
+    
     class AutoHelpConnection(connection):
         def on_chat(self, value, global_message):
             if deuce_howto_match(self, value):
-                self.send_chat("TO CHANGE YOUR NAME: Start Menu-> "
-                    "All Programs-> Ace of Spades-> Configuration")
-                self.protocol.irc_say("* Sent help to %s" % self.name)
+                reactor.callLater(1.0, send_help, self)
             return connection.on_chat(self, value, global_message)
     return protocol, AutoHelpConnection
