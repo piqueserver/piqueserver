bc024c5e6142036aaa16d74ba3598a3888031780
duckslingers <duckslingers@gmail.com>
2011-06-26 13:49:38 -0300

    Silly fix

--- feature_server/commands.py
+++ feature_server/commands.py
@@ -446,8 +446,8 @@ def ping(connection, value = None):
         player = get_player(connection.protocol, value)
     ping = int(player.latency * 1000) or 0
     if value is None:
-        return 'Your ping is %s ms. Lower ping is better, with best values '
-            'around 100-200' % (player.name, ping)
+        return ('Your ping is %s ms. Lower ping is better, with best values '
+            'around 100-200' % (player.name, ping))
     return "%s's ping is %s ms" % (player.name, ping)
 
 def intel(connection):
