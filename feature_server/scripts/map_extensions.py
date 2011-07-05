e10908fbf2df5f1eac12e9d99e7c5fc332f7c852
duckslingers <duckslingers@gmail.com>
2011-07-05 01:35:13 -0300

    map_extensions was swallowing the event

--- feature_server/scripts/map_extensions.py
+++ feature_server/scripts/map_extensions.py
@@ -1,25 +1,23 @@
+from pyspades.constants import HIT_CONSTANTS
 from pyspades.server import hit_packet
 
 def apply_script(protocol, connection, config):
-    
     class MapExtensionConnection(connection):
         def on_update_position(self):
-            extensions = self.protocol.map_info.extensions
-            if (extensions.has_key('water_damage') and
+            water_damage = self.protocol.map_info.extensions['water_damage']
+            if (self.speed_limit_grace<1 and
+                water_damage and
                 self.position.z>=61):
-                water_damage = extensions['water_damage']                
-                self.environment_hit(water_damage)
-    
-        def environment_hit(self, value):
-            if value<0 and self.hp >= 100: # do nothing at max health
-                return
+                self.environment_hit()
+            connection.on_update_position(self)
+        
+        def environment_hit(self):
+            value = 25
             self.hp -= value
             if self.hp <= 0:
                 self.kill()
                 return
-            if self.hp > 100: # limit health gains
-                self.hp = 100
-            hit_packet.hp = self.hp
+            hit_packet.value = HIT_CONSTANTS[value]
             self.send_contained(hit_packet)
-
+    
     return protocol, MapExtensionConnection
