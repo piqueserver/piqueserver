584cb76dd2dc8f90f0ed1820e453ec5485f55d41
James Hofmann <jhofmann@321f.net>
2011-07-04 17:27:05 -0700

    update water damage for 0.54 and add water healing feature

--- feature_server/scripts/map_extensions.py
+++ feature_server/scripts/map_extensions.py
@@ -1,24 +1,25 @@
-from pyspades.constants import HIT_CONSTANTS
 from pyspades.server import hit_packet
 
 def apply_script(protocol, connection, config):
     
     class MapExtensionConnection(connection):
         def on_update_position(self):
-            water_damage = self.protocol.map_info.extensions['water_damage']
-            
-            if (self.speed_limit_grace<1 and
-                water_damage and
+            extensions = self.protocol.map_info.extensions
+            if (extensions.has_key('water_damage') and
                 self.position.z>=61):
-                self.environment_hit()
+                water_damage = extensions['water_damage']                
+                self.environment_hit(water_damage)
     
-        def environment_hit(self):
-            value = 25
+        def environment_hit(self, value):
+            if value<0 and self.hp >= 100: # do nothing at max health
+                return
             self.hp -= value
             if self.hp <= 0:
                 self.kill()
                 return
-            hit_packet.value = HIT_CONSTANTS[value]
+            if self.hp > 100: # limit health gains
+                self.hp = 100
+            hit_packet.hp = self.hp
             self.send_contained(hit_packet)
 
     return protocol, MapExtensionConnection
