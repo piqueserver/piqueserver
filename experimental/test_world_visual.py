bdc4ed72ece8ccee25d6190f1665639d3188d40f
Mathias Kaerlev <matpow2@gmail.com>
2011-07-09 06:42:04 +0200

    Forgot silly GPL headers

--- experimental/test_world_visual.py
+++ experimental/test_world_visual.py
@@ -1,3 +1,20 @@
+# Copyright (c) Mathias Kaerlev 2011.
+
+# This file is part of pyspades.
+
+# pyspades is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# pyspades is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pyspades.  If not, see <http://www.gnu.org/licenses/>.
+
 from pyspades.common import *
 from pyspades.load import VXLData, get_color_tuple
 from pyspades import world
