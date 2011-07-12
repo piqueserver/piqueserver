f962487dd4e320b14b822d74200b53d5110b021c
Mathias Kaerlev <matpow2@gmail.com>
2011-07-12 02:45:09 +0200

    Fix game logic scripts for Enari

--- experimental/test_world.py
+++ experimental/test_world.py
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with pyspades.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
+sys.path.append('..')
+
 from pyspades.common import *
 from pyspades.load import VXLData
 from pyspades import world
