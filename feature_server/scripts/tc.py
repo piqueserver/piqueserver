4d9dcea9af2919a3da178f5aabe643d2225db755
James Hofmann <jhofmann@321f.net>
2011-08-18 04:59:05 -0700

    tc now defaults each half of the map to an owner; also various drawing improvements

--- feature_server/scripts/tc.py
+++ feature_server/scripts/tc.py
@@ -18,6 +18,23 @@ def score(connection):
         return ('You control %s sector(s), Green controls %s sector(s).' %
           (b_add, g_add))
     
+def cap_box_shape(grid_x, grid_y, is_green):
+    if is_green:
+        xstart = grid_x * 64 + 57
+    else:
+        xstart = grid_x * 64 + 2
+    xend = xstart + 6
+    ystart = grid_y * 64 + 2
+    yend = ystart + 6
+
+    result = []
+    
+    for x in xrange(xstart, xend):
+        for y in xrange(ystart, yend):
+            if (x == xstart or x == xend - 1 or
+                y == ystart or y == yend - 1):
+                result.append((x,y))
+    return result
 
 add(score)
 
@@ -45,79 +62,21 @@ def apply_script(protocol, connection, config):
             self.do_control(x, y)
             return connection.on_block_removed(self, x, y, z)
 
-        def draw_cap_box(self, grid_x, grid_y, color):
-            block_action.value = BUILD_BLOCK
-
-            # assign different fake players to different teams:
-            
-            if color[1] == 255:
-                player_id = 32
-                xstart = grid_x * 64 + 57
+        def clear_cap_box(self, x, y, is_green):
+            if is_green:
+                id = 33
             else:
-                player_id = 33
-                xstart = grid_x * 64 + 2
-                
-            xend = xstart + 6
-            ystart = grid_y * 64 + 2
-            yend = ystart + 6
-            
-            set_color.value = make_color(*color)
-            set_color.player_id = player_id
-            set_color.fog = False
-            self.protocol.send_contained(set_color,
-                                         save = True)
-            if self.protocol.god_blocks is None:
-                self.protocol.god_blocks = set()
-            for x in xrange(xstart, xend):
-                for y in xrange(ystart, yend):
-                    if (x == xstart or x == xend - 1 or
-                        y == ystart or y == yend - 1):
-                        block_action.x = x
-                        block_action.y = y
-                        block_action.z = 0
-                        block_action.player_id = player_id
-                        self.protocol.send_contained(block_action,
-                                                     save = True)
-                        self.protocol.map.set_point(x, y, 0, color + (255,),
-                            user = False)
-                        self.protocol.god_blocks.add((x, y, 0))
-            self.protocol.send_contained(set_color,
-                                         save = True)
-
-        def clear_cap_box(self, grid_x, grid_y):
-            block_action.value = DESTROY_BLOCK
-            cell = grid_x + grid_y * 8
-            xstart = grid_x * 64 + 2
-            xend = xstart + 6
-            ystart = grid_y * 64 + 2
-            yend = ystart + 6
-            for x in xrange(xstart, xend):
-                for y in xrange(ystart, yend):
-                    if (x == xstart or x == xend - 1 or
-                        y == ystart or y == yend - 1):
-                        # left
-                        block_action.x = x
-                        block_action.y = y
-                        block_action.z = 0
-                        block_action.player_id = 32
-                        self.protocol.send_contained(block_action,
-                                                     save = True)
-                        self.protocol.map.remove_point(x, y, 0,
-                            user = False)
-                        # right
-                        block_action.x = x + 55
-                        block_action.y = y
-                        block_action.z = 0
-                        block_action.player_id = 32
-                        self.protocol.send_contained(block_action,
-                                                     save = True)
-                        self.protocol.map.remove_point(x + 55, y, 0,
-                            user = False)
-            
-        def clear_cap_boxes(self):
-            for x in xrange(0, 8):
-                for y in xrange(0, 8):
-                    self.clear_cap_box(x, y)
+                id = 32
+            self.protocol.remove_shape(cap_box_shape(x, y, is_green), id)
+        
+        def draw_cap_box(self, x, y, is_green):
+            if is_green:
+                id = 33
+                color = (0, 255, 0)
+            else:
+                id = 32
+                color = (0, 0, 255)
+            self.protocol.draw_shape(cap_box_shape(x, y, is_green), id, color)
             
         def do_control(self, x, y):
             if self.protocol.get_owner(x, y) is self.team:
@@ -128,46 +87,49 @@ def apply_script(protocol, connection, config):
                     self.protocol.set_owner(x, y, self.team)) 
                 gridlocale = 'ABCDEFGH'[x//64] + str((y//64)+1)
 
-                nomans = (old_blue_held <
-                          self.protocol.min_blocks_to_capture and
-                          old_green_held <
-                          self.protocol.min_blocks_to_capture)
-
                 if self.team is self.protocol.green_team:
                     my_owned = self.protocol.green_tc_held[grididx]
                     other_owned = self.protocol.blue_tc_held[grididx]
-                    can_lose = (old_blue_held>=
-                        self.protocol.min_blocks_to_capture)
-                    can_cap = old_green_held<=old_blue_held or nomans
+                    own_turf = x//64 >= 4
+                    other_advantage = old_green_held<=old_blue_held
+                    other_was_min = (old_blue_held >=
+                                 self.protocol.min_blocks_to_capture)
                 else:
                     my_owned = self.protocol.blue_tc_held[grididx]
                     other_owned = self.protocol.green_tc_held[grididx]
-                    can_lose = (old_green_held>=
-                        self.protocol.min_blocks_to_capture)
-                    can_cap = old_green_held>=old_blue_held or nomans
+                    own_turf = x//64 < 4
+                    other_advantage = old_green_held>=old_blue_held
+                    was_under_min = (old_blue_held <
+                                     self.protocol.min_blocks_to_capture)
+                    other_was_min = (old_green_held >=
+                                 self.protocol.min_blocks_to_capture)
+                    
+                have_min = (my_owned >=
+                             self.protocol.min_blocks_to_capture)
+                other_min = (other_owned >=
+                             self.protocol.min_blocks_to_capture)
+                have_advantage = my_owned > other_owned
+                can_cap = ((own_turf and other_advantage and other_was_min) or
+                           (not own_turf and
+                            (other_advantage or was_under_min)))
 
-                if (my_owned>other_owned and
-                    my_owned>=self.protocol.min_blocks_to_capture):
-                    if can_cap:                    
+                if ((have_advantage and have_min) or (own_turf and
+                                                      not other_min)):
+                    if can_cap:
                         if self.team is self.protocol.green_team:
                             self.protocol.send_chat('GREEN has captured %s' %
                                                     gridlocale)
-                            self.clear_cap_box(x//64,y//64)
-                            self.draw_cap_box(x//64,y//64,(0,255,0))
+                            self.clear_cap_box(x//64,y//64, False)
+                            self.draw_cap_box(x//64,y//64, True)
                         else:
                             self.protocol.send_chat('BLUE has captured %s' %
                                                     gridlocale)
-                            self.clear_cap_box(x//64,y//64)
-                            self.draw_cap_box(x//64,y//64,(0,0,255))
+                            self.clear_cap_box(x//64,y//64, True)
+                            self.draw_cap_box(x//64,y//64, False)
                     else:
                         self.squelch_chat('owner', gridlocale, my_owned,
                                           other_owned)
                 else:
-                    if (can_lose and
-                        other_owned<self.protocol.min_blocks_to_capture):
-                        self.clear_cap_box(x//64,y//64)
-                        self.protocol.send_chat('%s is NO-MANS-LAND!' %
-                                                gridlocale)
                     self.squelch_chat('attacker', gridlocale, my_owned,
                                       other_owned)
                    
@@ -182,11 +144,12 @@ def apply_script(protocol, connection, config):
                     'You own %s with %s blocks (Enemy: %s)' %
                    (gridlocale, my_owned, other_owned))
                 elif style == 'attacker':
-                    if my_owned > self.protocol.min_blocks_to_capture:
+                    if (my_owned > self.protocol.min_blocks_to_capture or
+                        other_owned > self.protocol.min_blocks_to_capture):
                         self.send_chat(
                         'You now control %s blocks of %s (Enemy: %s)' %
                         (my_owned, gridlocale, other_owned))
-                    else:
+                    else:                        
                         self.send_chat(
                         'You now control %s blocks of %s (%s blocks to cap)' %
                     (my_owned, gridlocale, self.protocol.min_blocks_to_capture))
@@ -202,7 +165,7 @@ def apply_script(protocol, connection, config):
         blue_tc_held = [0 for n in xrange(8*8)]
         territory_update_time = config.get('territory_update_time', 30)
         score_limit = config.get('score_limit', 100)
-        min_blocks_to_capture = config.get('min_blocks_to_capture', 10)
+        min_blocks_to_capture = config.get('min_blocks_to_capture', 30)
         tc_owner = array('i')
         for y in xrange(512):
             for x in xrange(512):
@@ -213,23 +176,94 @@ def apply_script(protocol, connection, config):
             self.gameplay_loop = LoopingCall(self.update_tc_score)
             self.gameplay_loop.start(self.territory_update_time)
             self.reset_ownership()
+            self.init_cap_boxes()
             return result
 
+        def draw_shape(self, shape, id, color):           
+            block_action.value = BUILD_BLOCK
+            
+            set_color.value = make_color(*color)
+            set_color.player_id = id
+            set_color.fog = False
+            self.send_contained(set_color,
+                                         save = True)
+            
+            if self.god_blocks is None:
+                self.god_blocks = set()          
+            
+            for pt in shape:
+                x, y = pt
+                self.god_blocks.add((x, y, 0))
+                self.map.set_point(x, y, 0, color + (255,),
+                                   user = False)
+                block_action.x = x
+                block_action.y = y
+                block_action.z = 0
+                block_action.player_id = id
+                self.send_contained(block_action,
+                                     save = True)
+
+        def remove_shape(self, shape, id):
+            block_action.value = DESTROY_BLOCK
+            for pt in shape:
+                x, y = pt
+                if (x, y, 0) in self.god_blocks:
+                    self.god_blocks.remove((x, y, 0))
+                self.map.remove_point(x, y, 0, user = False)
+                block_action.x = x
+                block_action.y = y
+                block_action.z = 0
+                block_action.player_id = id
+                self.send_contained(block_action,
+                                     save = True)
+                
+        def init_cap_boxes(self, player = None):
+            if self.god_blocks is None:
+                self.god_blocks = set()          
+            
+            for x in xrange(4):
+                for y in xrange(8):
+                    addshape = cap_box_shape(x, y, False)
+                    if not self.map.get_solid(*(addshape[0]+(0,))):
+                        self.draw_shape(addshape, 32, (0, 0, 255))
+                    delshape = cap_box_shape(x, y, True)
+                    if self.map.get_solid(*(delshape[0]+(0,))):
+                        self.remove_shape(delshape, 33)
+                    addshape = cap_box_shape(x + 4, y, True)
+                    if not self.map.get_solid(*(addshape[0]+(0,))):
+                        self.draw_shape(addshape, 34, (0, 255, 0))
+                    delshape = cap_box_shape(x + 4, y, False)
+                    if self.map.get_solid(*(delshape[0]+(0,))):
+                        self.remove_shape(delshape, 35)
+ 
         def compute_tc_score(self):
             g_add = 0
             b_add = 0
+            x = 0
             for cell in xrange(len(self.blue_tc_held)):
-                if (self.blue_tc_held[cell]>self.green_tc_held[cell] and
-                    self.blue_tc_held[cell]>=self.min_blocks_to_capture):
+                blue_turf = x < 4
+                green_turf = x >= 4
+                blue_majority = (self.blue_tc_held[cell] >=
+                                 self.green_tc_held[cell])
+                green_majority = not blue_majority
+                blue_min_threshold = (self.blue_tc_held[cell] >=
+                                      self.min_blocks_to_capture)
+                green_min_threshold = (self.green_tc_held[cell] >=
+                                      self.min_blocks_to_capture)
+                if ((blue_turf and blue_majority) or
+                    (blue_turf and not green_min_threshold) or
+                    (blue_min_threshold and blue_majority)):
                     b_add += 1
-                elif (self.blue_tc_held[cell]<self.green_tc_held[cell] and
-                    self.green_tc_held[cell]>=self.min_blocks_to_capture):
+                elif ((green_turf and green_majority) or
+                    (green_turf and not blue_min_threshold) or
+                    (green_min_threshold and green_majority)):
                     g_add += 1
+                x = (x + 1) % 8
             return g_add, b_add
         
         def update_tc_score(self):
             g_add, b_add = self.compute_tc_score()
-            gpos = 4 + (g_add - b_add)
+            gpos = 4 + (g_add - b_add)//2
             if gpos < 0:
                 gpos = 0
             if gpos > 8:
@@ -336,7 +370,7 @@ def apply_script(protocol, connection, config):
             self.green_tc_held = [0 for n in xrange(8*8)]
             self.blue_tc_held = [0 for n in xrange(8*8)]
             self.reset_ownership()
-            player.clear_cap_boxes()
+            self.init_cap_boxes(player)
             return protocol.on_game_end(self, player)
 
         def get_a_player(self, team):
