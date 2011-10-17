2355b57da06d3b14d3d7570651304ca358ae04ea
enari <enari@minit.nu>
2011-10-17 20:13:48 +0000

    Excluded intelcaps for /ratio

--- contrib/scripts/ratio.py
+++ contrib/scripts/ratio.py
@@ -7,16 +7,17 @@ def ratio(connection, user=None):
         connection = commands.get_player(connection.protocol, user)
     if connection not in connection.protocol.players:
         raise KeyError()
-    ratio = connection.kills/float(max(1,connection.deaths))
+    ratio = connection.ratio_kills/float(max(1,connection.ratio_deaths))
     ratio_msg = "You have a kill-death ratio of %.2f" % ratio
     return ('%s (%s kills, %s deaths).' %
-        (ratio_msg, connection.kills, connection.deaths))
+        (ratio_msg, connection.ratio_kills, connection.ratio_deaths))
 
 commands.add(ratio)
 
 def apply_script(protocol, connection, config):
     class RatioConnection(connection):
-        deaths = 0
+        ratio_kills = 0
+        ratio_deaths = 0
         
         def on_kill(self, killer):
             ret = connection.on_kill(self, killer)
@@ -24,6 +25,9 @@ def apply_script(protocol, connection, config):
             if ret:
                 return ret
             
-            self.deaths += 1
+            if self != killer:
+                killer.ratio_kills += 1
+            
+            self.ratio_deaths += 1
     
     return protocol, RatioConnection
