be4627b8e97eccc6f04e653703a635e065922696
James Hofmann <jhofmann@321f.net>
2011-12-16 12:55:30 -0800

    izzy's request: add fake "ADMIN" detect to antijerk

--- feature_server/scripts/antijerk.py
+++ feature_server/scripts/antijerk.py
@@ -5,9 +5,12 @@ chat_pattern = re.compile(".*(airstrike).*(esc|escape|alt-f4|alt f4)",
     re.IGNORECASE)
 chat_pattern_2 = re.compile(".*(esc|escape|alt-f4|alt f4).*(airstrike)",
     re.IGNORECASE)
+admin_pattern = re.compile(".*(admin)",
+    re.IGNORECASE)
     
 def antijerk_match(player, msg):
-    return chat_pattern.match(msg) or chat_pattern_2.match(msg)
+    return chat_pattern.match(msg) or chat_pattern_2.match(msg) or\
+           admin_pattern.match(player.name)
     
 def apply_script(protocol, connection, config):
     def jerk_kick(connection):
@@ -23,5 +26,10 @@ def apply_script(protocol, connection, config):
                 jerk_kick(self)
             else:
                 return connection.on_chat(self, value, global_message)
+        def on_login(self, name):
+            if admin_pattern.match(name):
+                self.send_chat('Anti-jerk: Please remove "Admin" from your '+
+                               'name: TALKING WILL AUTOBAN YOU!')
+            return connection.on_login(self, name)
                 
     return protocol, AntiJerkConnection
