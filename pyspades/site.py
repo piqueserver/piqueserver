8666d82f390c4f7a92c7bca994aa176d8b416ece
Mathias Kaerlev <matpow2@gmail.com>
2011-12-30 21:24:24 +0100

    Update site (and add proper site parser)

--- pyspades/site.py
+++ pyspades/site.py
@@ -2,32 +2,47 @@ from twisted.web.client import getPage
 from twisted.internet.defer import Deferred
 from collections import namedtuple
 from pyspades.tools import get_server_ip
+from HTMLParser import HTMLParser
 
-SERVER_LIST = 'http://ace-spades.com/?page_id=5'
-
-START_IDENTIFIER = '<pre>#/MAX PING CNTRY NAME (Click to Join)\n'
-END_IDENTIFIER = '</pre>'
+SERVER_LIST = 'http://ace-spades.com/serverlist.txt'
 
 ServerEntry = namedtuple('ServerEntry', ['name', 'ip', 'ping', 'players', 
     'max', 'country'])
 
+class ServerParser(HTMLParser):
+    data_index = 0
+    
+    def reset_server(self):
+        self.data_index = 0
+    
+    def handle_starttag(self, tag, attrs):
+        if tag == 'img':
+            # country
+            url = attrs[0][1]
+            self.country = url.split('/')[-1][1:-4]
+        elif tag == 'a':
+            # identifier (aos://12345678)
+            self.ip = get_server_ip(attrs[0][1])
+    def handle_data(self, data):
+        if self.data_index == 0:
+            players, ping = data.rsplit(None, 1)
+            count, max = players.split('/')
+            self.players = int(count.strip())
+            self.max = int(max.strip())
+            self.ping = int(ping.strip())
+        elif self.data_index == 2:
+            self.name = data
+        self.data_index += 1
+
 def got_servers(data, defer):
-    start = data.index(START_IDENTIFIER) + len(START_IDENTIFIER)
-    end = data.index(END_IDENTIFIER, start)
-    data = data[start:end].splitlines()
+    data = data.splitlines()
     servers = []
+    parser = ServerParser()
     for value in data:
-        players, max = value[0:5].strip().split('/')
-        players = int(players)
-        max = int(max)
-        ping = int(value[5:10].strip())
-        country = value[48:50]
-        identifier = value[70:]
-        end_start = identifier.index('>')+1
-        real_identifier = int(identifier[15:end_start-2])
-        ip = get_server_ip(real_identifier)
-        name = identifier[end_start:-4]
-        servers.append(ServerEntry(name, ip, ping, players, max, country))
+        parser.reset_server()
+        parser.feed(value)
+        servers.append(ServerEntry(parser.name, parser.ip, parser.ping, 
+            parser.players, parser.max, parser.country))
     defer.callback(servers)
 
 def get_servers():
