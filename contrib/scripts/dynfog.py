b2aefaa74db63dfd14fe979aaa1df27804435b6c
snollygolly <devnull@localhost>
2012-01-19 20:05:15 -0600

    fixed bug in dynfog

--- contrib/scripts/dynfog.py
+++ contrib/scripts/dynfog.py
@@ -4,8 +4,6 @@ def apply_script(protocol, connection, config):
     class FogProtocol(protocol):
         default_fog = (128, 232, 255)
         def on_map_change(self, name):
-            if hasattr(self.map_info.info, 'fog'):
-                self.set_fog_color(getattr(self.map_info.info, 'fog'))
-            else:
-                self.set_fog_color(self.default_fog)
+            self.set_fog_color(getattr(self.map_info.info, 'fog', self.default_fog))
+            protocol.on_map_change(self, name) 
     return FogProtocol, connection
