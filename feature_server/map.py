777530170eec9fac36a2d6ca07663a0615199f31
James Hofmann <jhofmann@321f.net>
2012-03-11 19:09:13 -0700

    refactor rotation info into a data structure

--- feature_server/map.py
+++ feature_server/map.py
@@ -27,52 +27,50 @@ DEFAULT_LOAD_DIR = './maps'
 class MapNotFound(IOError):
     pass
 
-def get_map_filename(name, load_dir = DEFAULT_LOAD_DIR):
-    return os.path.join(load_dir, '%s.vxl' % name)
-
-def get_meta_filename(name, load_dir = DEFAULT_LOAD_DIR):
-    return os.path.join(load_dir, '%s.txt' % name)
+def parse_mapname(name):
+    seedsplit = name.split("#")
+    if len(seedsplit) > 1: # user specified a seed
+        basename = seedsplit[0].strip()
+        seed = int(seedsplit[1])
+    else: # manufacture a reproducable seed value
+        basename = name
+        random.seed()
+        seed = random.randint(0, math.pow(2, 31))
+    return RotationInfo(basename, seed)
 
 def check_rotation(maps, load_dir = DEFAULT_LOAD_DIR):
     for map in maps:
-        map = map.split("#")[0].strip()
-        if (not os.path.isfile(get_map_filename(map, load_dir)) 
-        and not os.path.isfile(get_meta_filename(map, load_dir))):
+        map = parse_mapname(map)
+        if (not os.path.isfile(map.map(load_dir))
+        and not os.path.isfile(map.meta(load_dir))):
             raise MapNotFound('map %s does not exist' % map)
 
 class Map(object):
     def __init__(self, name, load_dir = DEFAULT_LOAD_DIR):
-        seedsplit = name.split("#")
-        if len(seedsplit) > 1: # user specified a seed
-            basename = seedsplit[0].strip()
-            seed = int(seedsplit[1])
-        else: # manufacture a reproducable seed value
-            basename = name
-            random.seed()
-            seed = random.randint(0, math.pow(2, 31))
+
+        rot_info = parse_mapname(name)
         
-        self.load_information(basename, load_dir)
+        self.load_information(rot_info, load_dir)
         
-        if self.gen_script:
-            # retitle the map with the seed
-            self.name = basename + " #%s" % seed
+        if self.gen_script and rot_info.seed is not None:
+            self.name = rot_info.name_seed()
             print "Generating map '%s'..." % self.name
-            random.seed(seed)
-            self.data = self.gen_script(basename, seed)
+            random.seed(rot_info.seed)
+            self.data = self.gen_script(rot_info.file, rot_info.seed)
         else:
             print "Loading map '%s'..." % self.name
-            self.load_vxl(basename, load_dir)
+            self.load_vxl(rot_info, load_dir)
 
         print 'Map loaded successfully.'
 
-    def load_information(self, name, load_dir):
-        info_file = get_meta_filename(name, load_dir)
+    def load_information(self, rot_info, load_dir):
         try:
-            info = imp.load_source(name, info_file)
+            info = imp.load_source(rot_info.name(), rot_info.meta())
         except IOError:
             info = None
         self.info = info
-        self.name = getattr(info, 'name', name)
+        self.rot_info = rot_info
+        self.name = getattr(info, 'name', self.rot_info.name())
         self.author = getattr(info, 'author', '(unknown)')
         self.version = getattr(info, 'version', '1.0')
         self.description = getattr(info, 'description', '')
@@ -87,16 +85,32 @@ class Map(object):
         self.on_map_leave = getattr(info, 'on_map_leave', None)
         self.on_block_destroy = getattr(info, 'on_block_destroy', None)
         self.is_indestructable = getattr(info, 'is_indestructable', None)
+
+        if self.gen_script:
+            self.name = self.rot_info.name_seed()
         
     def apply_script(self, protocol, connection, config):
         if self.script is not None:
             protocol, connection = self.script(protocol, connection, config)
         return protocol, connection
 
-    def load_vxl(self, name, load_dir):
+    def load_vxl(self, rot_info, load_dir):
         try:
-            fp = open(get_map_filename(name, load_dir), 'rb')
+            fp = open(rot_info.map(load_dir), 'rb')
         except OSError:
-            raise MapNotFound('map %s does not exist' % name)
+            raise MapNotFound('map %s does not exist' % rot_info.name())
         self.data = VXLData(fp)
         fp.close()
+
+class RotationInfo(object):
+    def __init__(self, file = "pyspades", seed = None):
+        self.file = file
+        self.seed = seed
+    def name(self):
+        return self.file
+    def name_seed(self):
+        return "%s #%s" % (self.file, self.seed)
+    def map(self, load_dir = DEFAULT_LOAD_DIR):
+        return os.path.join(load_dir, '%s.vxl' % self.file)
+    def meta(self, load_dir = DEFAULT_LOAD_DIR):
+        return os.path.join(load_dir, '%s.txt' % self.file)
