3802df4ff1d30c12794792eabeb6ba89849c19a7
Ben Aksoy <aksoy.ben@gmail.com>
2012-03-13 09:36:48 +0000

    Bumped server and game versions (protocol update)

--- pyspades/constants.py
+++ pyspades/constants.py
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with pyspades.  If not, see <http://www.gnu.org/licenses/>.
 
-MASTER_VERSION = 30
-GAME_VERSION = 2
+MASTER_VERSION = 31
+GAME_VERSION = 3
 RIFLE_WEAPON, SMG_WEAPON, SHOTGUN_WEAPON = xrange(3)
 SEMI_WEAPON = RIFLE_WEAPON
 TORSO, HEAD, ARMS, LEGS, MELEE = xrange(5)
