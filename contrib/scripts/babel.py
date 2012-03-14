dc7eec26c87b517056769f2d2938b2897c898bdb
Yourself <spacewebguy@gmail.com>
2012-03-14 18:59:55 -0500

    Updated babel for 0.75

--- contrib/scripts/babel.py
+++ contrib/scripts/babel.py
@@ -1,9 +1,22 @@
+# Tower of Babel created by Yourself, modified by izzy
+#    how to install:
+# set game_mode to ctf in config.txt
+# add babel to script list in config.txt
+# add to map .txt files: extensions = { 'babel' : True }
+#    additional suggestions:
+# add onectf to script list in config.txt http://pyspades.googlecode.com/hg/contrib/scripts/onectf.py
+# set cap_limit to 10 in config.txt
+
 from pyspades.constants import *
 from random import randint
 
+# If ALWAYS_ENABLED is False, then babel can be enabled by setting 'babel': True
+# in the map metadat extensions dictionary.
+ALWAYS_ENABLED = True
+
 PLATFORM_WIDTH = 100
 PLATFORM_HEIGHT = 32
-PLATFORM_COLOR = (128, 128, 128, 255)
+PLATFORM_COLOR = (255, 255, 255, 255)
 BLUE_BASE_COORDS = (256-138, 256)
 GREEN_BASE_COORDS = (256+138, 256)
 SPAWN_SIZE = 40
@@ -32,8 +45,13 @@ def get_spawn_location(connection):
     return (xb, yb, connection.protocol.map.get_z(xb, yb))
 
 def coord_on_platform(x, y, z):
-    if x >= (256 - PLATFORM_WIDTH) and x <= (256 + PLATFORM_WIDTH) and y >= (256 - PLATFORM_HEIGHT) and y <= (256 + PLATFORM_HEIGHT):
-        return True
+    if z <= 2:
+        if x >= (256 - PLATFORM_WIDTH) and x <= (256 + PLATFORM_WIDTH) and y >= (256 - PLATFORM_HEIGHT) and y <= (256 + PLATFORM_HEIGHT):
+            return True
+    if z == 1:
+        if x >= (256 - PLATFORM_WIDTH - 1) and x <= (256 + PLATFORM_WIDTH + 1) \
+           and y >= (256 - PLATFORM_HEIGHT - 1) and y <= (256 + PLATFORM_HEIGHT + 1):
+            return True
     return False
 
 def apply_script(protocol, connection, config):
@@ -41,10 +59,13 @@ def apply_script(protocol, connection, config):
         babel = False 
         def on_map_change(self, map):
             extensions = self.map_info.extensions
-            if extensions.has_key('babel'):
-                self.babel = extensions['babel']
+            if ALWAYS_ENABLED:
+                self.babel = True
             else:
-                self.babel = False
+                if extensions.has_key('babel'):
+                    self.babel = extensions['babel']
+                else:
+                    self.babel = False
             if self.babel:
                 self.map_info.cap_limit = 1
                 self.map_info.get_entity_location = get_entity_location
@@ -56,24 +77,67 @@ def apply_script(protocol, connection, config):
         
         def is_indestructable(self, x, y, z):
             if self.babel:
-                if z == 1:
-                    if coord_on_platform(x, y, z):
-                        protocol.is_indestructable(self, x, y, z)
-                        return True
+                if coord_on_platform(x, y, z):
+                    protocol.is_indestructable(self, x, y, z)
+                    return True
             return protocol.is_indestructable(self, x, y, z)
     
     class BabelConnection(connection):
-        def on_block_build_attempt(self, x, y, z):
+        def invalid_build_position(self, x, y, z):
             if not self.god and self.protocol.babel:
-                if z <= 2:
-                    if coord_on_platform(x, y, z):
-                        connection.on_block_build_attempt(self, x, y, z)
-                        return False
-                if z == 1:
-                    if x >= (256 - PLATFORM_WIDTH - 1) and x <= (256 + PLATFORM_WIDTH + 1) \
-                       and y >= (256 - PLATFORM_HEIGHT -1) and y <= (256 + PLATFORM_HEIGHT + 1):
-                        connection.on_block_build_attempt(self, x, y, z)
-                        return False
+                if coord_on_platform(x, y, z):
+                    connection.on_block_build_attempt(self, x, y, z)
+                    return True
+            # prevent enemies from building in protected areas
+            if self.team is self.protocol.blue_team:
+                if self.world_object.position.x >= 301 and self.world_object.position.x <= 384 \
+                    and self.world_object.position.y >= 240 and self.world_object.position.y <= 272:
+                        self.send_chat('You can\'t build near the enemy\'s tower!')
+                        return True
+            if self.team is self.protocol.green_team:
+                if self.world_object.position.x >= 128 and self.world_object.position.x <= 211 \
+                    and self.world_object.position.y >= 240 and self.world_object.position.y <= 272:
+                        self.send_chat('You can\'t build near the enemy\'s tower!')
+                        return True
+            return False
+
+        def on_block_build_attempt(self, x, y, z):
+            if invalid_build_position(x, y, z):
+                return False
             return connection.on_block_build_attempt(self, x, y, z)
-    
+
+        def on_line_build_attempt(self, points):
+            for point in points:
+                if invalid_build_position(point.x, point.y, point.z):
+                    return False
+            return connection.on_line_build_attempt(self, points)
+
+        # anti team destruction
+        def on_block_destroy(self, x, y, z, mode):
+            if self.team is self.protocol.blue_team:
+                if self.tool is SPADE_TOOL and self.world_object.position.x >= 128 and self.world_object.position.x <= 211 \
+                    and self.world_object.position.y >= 240 and self.world_object.position.y <= 272:
+                        self.send_chat('You can\'t destroy your team\'s blocks in this area. Attack the enemy\'s tower!')
+                        return False
+                if self.world_object.position.x <= 288:
+                    if self.tool is WEAPON_TOOL:
+                        self.send_chat('You must be closer to the enemy\'s base to shoot blocks!')
+                        return False
+                    if self.tool is GRENADE_TOOL:
+                        self.send_chat('You must be closer to the enemy\'s base to grenade blocks!')
+                        return False
+            if self.team is self.protocol.green_team:
+                if self.tool is SPADE_TOOL and self.world_object.position.x >= 301 and self.world_object.position.x <= 384 \
+                    and self.world_object.position.y >= 240 and self.world_object.position.y <= 272:
+                        self.send_chat('You can\'t destroy your team\'s blocks in this area. Attack the enemy\'s tower!')
+                        return False
+                if self.world_object.position.x >= 224:
+                    if self.tool is WEAPON_TOOL:
+                        self.send_chat('You must be closer to the enemy\'s base to shoot blocks!')
+                        return False
+                    if self.tool is GRENADE_TOOL:
+                        self.send_chat('You must be closer to the enemy\'s base to grenade blocks!')
+                        return False
+            return connection.on_block_destroy(self, x, y, z, mode)
+   
     return BabelProtocol, BabelConnection
