def94d28d629e57c600eff233868db5311ba7788
Yourself <spacewebguy@gmail.com>
2012-04-02 17:15:54 -0500

    onectf.py rewritten

--- contrib/scripts/onectf.py
+++ contrib/scripts/onectf.py
@@ -2,36 +2,23 @@ from pyspades.constants import *
 from commands import add, admin
 from pyspades.collision import vector_collision
 
-CENTER_X = 256
-CENTER_Y = 256
+FLAG_SPAWN_POS = (256, 256)
 
-HIDE_X = 0
-HIDE_Y = 0
-HIDE_Z = 63
+HIDE_POS = (0, 0, 63)
 
 # 1CTF and R1CTF can be enabled via the map metadata. Enable it by setting
-# 'game_script_mode' to '1CTF' or 'R1CTF' in the extensions dictionary. ex:
-# extensions = {'game_script_mode': '1CTF'}
+# 'one_ctf' to 'True' or 'reverse_one_ctf' to 'True' in the extensions dictionary. ex:
+# extensions = {'reverse_one_ctf': True}
 
-# If ALWAYS_ENABLED is True, then 1CTF will always be enabled
-# and map metadata doesn't need to be used
-ALWAYS_ENABLED = False
+DISABLED, ONE_CTF, REVERSE_ONE_CTF = xrange(3)
 
-# If ALWAYS_ENABLED_REVERSED is True, then R1CTF will always be enabled
-# and map metadata doesn't need to be used
-ALWAYS_ENABLED_REVERSED = False
+ONE_CTF_MODE = REVERSE_ONE_CTF
 
 # In reverse ctf, the goal is to take the intel to the enemy base
 
 # The message to send when a player takes the intel to the wrong base
 # when playing reverse ctf
-REVERSE_CTF_MESSAGE = 'Take the intel to the enemy base to score.'
-
-@admin
-def resetflags(connection):
-    connection.protocol.reset_flags()
-
-add(resetflags)
+REVERSE_ONE_CTF_MESSAGE = 'Take the intel to the enemy base to score.'
 
 def apply_script(protocol, connection, config):
     game_mode = config.get('game_mode', 'ctf')
@@ -39,57 +26,58 @@ def apply_script(protocol, connection, config):
         return protocol, connection
 
     class OneCTFProtocol(protocol):
-        map_changed = False
-        reverse_one_ctf = False
         one_ctf = False
+        reverse_one_ctf = False
 
-        def __init__(self, *arg, **kw):
-            protocol.__init__(self, *arg, **kw)
-            self.reset_flags()
+        def onectf_reset_flag(self, flag):
+            z = self.map.get_z(*self.one_ctf_spawn_pos)
+            pos = (self.one_ctf_spawn_pos[0], self.one_ctf_spawn_pos[1], z)
+            if flag is not None:
+                flag.player = None
+                flag.set(*pos)
+                flag.update()
+            return pos
 
-        def reset_flags(self):
+        def onectf_reset_flags(self):
             if self.one_ctf or self.reverse_one_ctf:
-                blue_flag = self.blue_team.flag
-                green_flag = self.green_team.flag
-                blue_flag.player = green_flag.player = None
-                z = self.map.get_z(CENTER_X, CENTER_Y)
-                blue_flag.set(CENTER_X, CENTER_Y, z)
-                green_flag.set(CENTER_X, CENTER_Y, z)
-                blue_flag.update()
-                green_flag.update()
+                self.onectf_reset_flag(self.blue_team.flag)
+                self.onectf_reset_flag(self.green_team.flag)
         
         def on_game_end(self):
             if self.one_ctf or self.reverse_one_ctf:
-                self.reset_flags()
+                self.onectf_reset_flags()
             return protocol.on_game_end(self)
-        
-        def on_map_change(self, m):
-            self.one_ctf = False
-            self.reverse_one_ctf = False
+
+        def on_map_change(self, map):
+            self.one_ctf = self.reverse_one_ctf = False
+            self.one_ctf_spawn_pos = FLAG_SPAWN_POS
             extensions = self.map_info.extensions
-            if ALWAYS_ENABLED:
+            if ONE_CTF_MODE == ONE_CTF:
                 self.one_ctf = True
-            elif ALWAYS_ENABLED_REVERSED:
+            elif ONE_CTF_MODE == REVERSE_ONE_CTF:
                 self.reverse_one_ctf = True
-            elif extensions.has_key('game_script_mode'):
-                value = extensions['game_script_mode']
-                if value == '1CTF':
-                    self.one_ctf = True
-                elif value == 'R1CTF':
-                    self.reverse_one_ctf = True
-            if self.one_ctf or self.reverse_one_ctf:
-                self.map_changed = True
-            return protocol.on_map_change(self, m)
+            elif extensions.has_key('one_ctf'):
+                self.one_ctf = extensions['one_ctf']
+            if not self.one_ctf and extensions.has_key('reverse_one_ctf'):
+                self.reverse_one_ctf = extensions['reverse_one_ctf']
+            if extensions.has_key('one_ctf_spawn_pos'):
+                self.one_ctf_spawn_pos = extensions['one_ctf_spawn_pos']
+            return protocol.on_map_change(self, map)
+
+        def on_flag_spawn(self, x, y, z, flag, entity_id):
+            pos = self.onectf_reset_flag(flag.team.other.flag)
+            protocol.on_flag_spawn(self, pos[0], pos[1], pos[2], flag, entity_id)
+            return pos
 
     class OneCTFConnection(connection):
         def on_flag_take(self):
             if self.protocol.one_ctf or self.protocol.reverse_one_ctf:
                 flag = self.team.flag
                 if flag.player is None:
-                    flag.set(HIDE_X, HIDE_Y, HIDE_Z)
+                    flag.set(*HIDE_POS)
                     flag.update()
                     if self.protocol.reverse_one_ctf:
-                        self.send_chat(REVERSE_CTF_MESSAGE)
+                        self.send_chat(REVERSE_ONE_CTF_MESSAGE)
                 else:
                     return False
             return connection.on_flag_take(self)
@@ -98,10 +86,7 @@ def apply_script(protocol, connection, config):
             if self.protocol.one_ctf or self.protocol.reverse_one_ctf:
                 flag = self.team.flag
                 position = self.world_object.position
-                x = int(position.x)
-                y = int(position.y)
-                z = int(position.z)
-                z = max(0, int(position.z))
+                x, y, z = int(position.x), int(position.y), max(0, int(position.z))
                 z = self.protocol.map.get_z(x, y, z)
                 flag.set(x, y, z)
                 flag.update()
@@ -117,20 +102,13 @@ def apply_script(protocol, connection, config):
 
         def capture_flag(self):
             if self.protocol.reverse_one_ctf:
-                self.send_chat(REVERSE_CTF_MESSAGE)
+                self.send_chat(REVERSE_ONE_CTF_MESSAGE)
                 return False
             return connection.capture_flag(self)
 
         def on_flag_capture(self):
             if self.protocol.one_ctf or self.protocol.reverse_one_ctf:
-                self.protocol.reset_flags()
+                self.protocol.onectf_reset_flags()
             return connection.on_flag_capture(self)
-        
-        def on_spawn(self, pos):
-            if self.protocol.one_ctf or self.protocol.reverse_one_ctf:
-                if self.protocol.map_changed == True:
-                    self.protocol.map_changed = False
-                    self.protocol.reset_flags()
-            return connection.on_spawn(self, pos)
     
     return OneCTFProtocol, OneCTFConnection
