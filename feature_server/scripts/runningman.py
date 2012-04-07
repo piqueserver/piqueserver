10c1fb1a9f4e82820a9a565d65b69777f1f07869
duckslingers <duckslingers@gmail.com>
2012-04-06 21:01:36 -0300

    More runningman updates

--- feature_server/scripts/runningman.py
+++ feature_server/scripts/runningman.py
@@ -1,8 +1,12 @@
 """
-Links two teammates so that they explode if they stray away from each other.
+Links any two teammates so that they explode if they stray away from each other.
 An incredibly unnerving method of encouraging teamplay.
 
 Enable by doing /runningman or by setting ENABLED_AT_START = True
+Linking is automatic at spawn and tries to find new partners every two deaths.
+Use /unlink [player] to toggle yourself or someone from being linked.
+
+May not work well with squads.
 
 Maintainer: hompy
 """
@@ -12,39 +16,41 @@ from twisted.internet.reactor import seconds
 from pyspades.world import Grenade
 from pyspades.server import grenade_packet
 from pyspades.collision import distance_3d_vector
+from pyspades.constants import *
 from commands import add, admin, name, get_player
 
 ENABLED_AT_START = False
 
-S_ENABLED = 'Running man mode ENABLED'
-S_DISABLED = 'Running man mode DISABLED'
+S_ENABLED = 'Running Man mode ENABLED'
+S_DISABLED = 'Running Man mode DISABLED'
 S_LINK_BREAK = "You strayed too far from {player}... don't go losing your head over it"
 S_LINK_WARNING = 'WARNING! Get back to {player} or you will both die!'
 S_LINKED = "You've been linked to {player}. Stay close to your partner or die!"
 S_FREE = "You're free to roam around... for now"
 S_LINKABLE = "{player}'s collar hums back to life"
+S_LINKABLE_SELF = "Your collar hums back to life"
 S_UNLINKABLE = '{player} is free as a bird'
-S_UNLINK_ALL = 'All players unlinked'
+S_UNLINKABLE_SELF = "You're free as a bird"
+S_UNLINK_ALL = 'All players unlinked!'
+S_FLAG_CAPTURED = 'The intel capture has unlinked everyone in the {team} team!'
 
 @admin
 @name('runningman')
 def running_man(connection):
     protocol = connection.protocol
     protocol.running_man = not protocol.running_man
-    if protocol.running_man:
-        protocol.send_chat(S_ENABLED, irc = True)
-    else:
-        protocol.send_chat(S_DISABLED, irc = True)
+    if not protocol.running_man:
         protocol.drop_all_links()
+    message = S_ENABLED if protocol.running_man else S_DISABLED
+    protocol.send_chat(message, irc = True)
 
 @admin
 def relink(connection):
     connection.protocol.drop_all_links()
     connection.protocol.send_chat(S_UNLINK_ALL, irc = True)
 
-@name('nolink')
 @admin
-def no_link(connection, player = None):
+def unlink(connection, player = None):
     protocol = connection.protocol
     if player is not None:
         player = get_player(protocol, player)
@@ -55,13 +61,13 @@ def no_link(connection, player = None):
     
     player.drop_link()
     player.linkable = not player.linkable
-    if player.linkable:
-        message = S_LINKABLE.format(player = player.name)
-    else:
-        message = S_UNLINKABLE.format(player = player.name)
-    protocol.send_chat(message, irc = True)
-
-for func in (running_man, relink, no_link):
+    player.send_chat(S_LINKABLE_SELF if player.linkable else S_UNLINKABLE_SELF)
+    message = S_LINKABLE if player.linkable else S_UNLINKABLE
+    message.format(player = player.name)
+    if connection is not player and connection in protocol.players:
+        return message
+    
+for func in (running_man, relink, unlink):
     add(func)
 
 def apply_script(protocol, connection, config):
@@ -86,17 +92,17 @@ def apply_script(protocol, connection, config):
                         self.send_chat(message)
                         message = S_LINK_BREAK.format(player = self.name)
                         self.link.send_chat(message)
-                    elif dist > self.protocol.link_warning_distance:
-                        if (self.last_warning is None or 
-                            seconds() - self.last_warning > 2.0):
-                            self.last_warning = seconds()
-                            self.link.last_warning = seconds()
-                            
-                            message = S_LINK_WARNING.format(
-                                player = self.link.name)
-                            self.send_chat(message)
-                            message = S_LINK_WARNING.format(player = self.name)
-                            self.link.send_chat(message)
+                    elif (dist > self.protocol.link_warning_distance and
+                        (self.last_warning is None or
+                        seconds() - self.last_warning > 2.0)):
+                        
+                        self.last_warning = seconds()
+                        self.link.last_warning = seconds()
+                        
+                        message = S_LINK_WARNING.format(player = self.link.name)
+                        self.send_chat(message)
+                        message = S_LINK_WARNING.format(player = self.name)
+                        self.link.send_chat(message)
             connection.on_position_update(self)
         
         def on_spawn(self, pos):
@@ -115,7 +121,11 @@ def apply_script(protocol, connection, config):
         
         def on_flag_capture(self):
             if self.protocol.running_man:
-                self.protocol.drop_all_links()
+                for player in self.protocol.players.values():
+                    if player.team is self.team:
+                        player.drop_link(no_message = True)
+            message = S_FLAG_CAPTURED.format(team = self.team.name)
+            self.protocol.send_chat(message, global_message = None)
             connection.on_flag_capture(self)
         
         def on_reset(self):
@@ -144,14 +154,14 @@ def apply_script(protocol, connection, config):
             self.link.link_deaths = 0
             
             message = S_LINKED.format(player = self.link.name)
-            self.send_chat(message % self.link.name)
+            self.send_chat(message)
             message = S_LINKED.format(player = self.name)
-            self.link.send_chat(message % self.name)
+            self.link.send_chat(message)
         
-        def drop_link(self, force_message = False):
+        def drop_link(self, force_message = False, no_message = False):
             if self.link is None:
                 return
-            if self.link.hp > 0 or force_message:
+            if (self.link.hp > 0 or force_message) and not no_message:
                 self.link.send_chat(S_FREE)
             self.link.link = None
             self.link = None
@@ -166,7 +176,7 @@ def apply_script(protocol, connection, config):
             grenade_packet.position = position.get()
             grenade_packet.velocity = (0.0, 0.0, 0.0)
             protocol.send_contained(grenade_packet)
-            self.kill()
+            self.kill(type = GRENADE_KILL)
     
     class RunningManProtocol(protocol):
         running_man = ENABLED_AT_START
@@ -176,7 +186,6 @@ def apply_script(protocol, connection, config):
         
         def drop_all_links(self):
             for player in self.players.values():
-                player.drop_link()
-                player.send_chat(S_FREE)
+                player.drop_link(no_message = True)
     
     return RunningManProtocol, RunningManConnection
