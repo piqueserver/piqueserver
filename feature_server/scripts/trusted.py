99fdcbe6b1c86989fca8eecff0fbb14d3531bb4d
duckslingers <duckslingers@gmail.com>
2012-04-14 11:37:42 -0300

    Granting trust also cancels an ongoing votekick

--- feature_server/scripts/trusted.py
+++ feature_server/scripts/trusted.py
@@ -2,18 +2,23 @@
 Adds the ability to 'trust' certain players, i.e. they cannot be votekicked
 or rubberbanded.
 
-Maintainer: mat^2
+Maintainer: mat^2 / hompy
 """
 
 from commands import add, admin, get_player
 
+S_GRANTED = '{player} is now trusted'
+S_GRANTED_SELF = "You've been granted trust, and can't be votekicked"
+S_LOGGED_IN = '{player} logged in as {user_type}'
+S_LOGGED_IN_SELF = 'Logged in as {user_type}'
+S_CANT_VOTEKICK = "{player} is trusted and can't be votekicked"
+
 @admin
 def trust(connection, player):
     player = get_player(connection.protocol, player)
-    player.trusted = True
-    player.speedhack_detect = False
-    player.send_chat("You're now a trusted user.")
-    return '%s is now trusted' % player.name
+    player.make_trusted()
+    player.send_chat(S_GRANTED_SELF)
+    return S_GRANTED.format(player = player.name)
 
 add(trust)
 
@@ -23,21 +28,24 @@ def apply_script(protocol, connection, config):
         
         def on_user_login(self, user_type):
             if user_type == 'trusted':
-                self.trusted = True
-                self.speedhack_detect = False
-                self.protocol.irc_say('* %s logged in as %s' %
-                    (self.name, user_type))
-                if self.protocol.votekick is not None and\
-                    self.protocol.votekick.target is self:
-                    self.protocol.votekick.cancel()
-                return 'Logged in as trusted user'
+                self.make_trusted()
+                self.protocol.irc_say('* ' + S_LOGGED_IN.format(
+                    player = self.name, user_type = user_type))
+                return S_LOGGED_IN_SELF.format(user_type = user_type)
             return connection.on_user_login(self, user_type)
+        
+        def make_trusted(self):
+            self.trusted = True
+            self.speedhack_detect = False
+            if (self.protocol.votekick is not None and
+                self.protocol.votekick.target is self):
+                self.protocol.votekick.cancel()
     
     class TrustedProtocol(protocol):
         def start_votekick(self, payload):
             player = payload.target
             if player.trusted:
-                return "%s is trusted and you can't votekick him." % player.name
+                return S_CANT_VOTEKICK.format(player = player.name)
             return protocol.start_votekick(self, payload)
     
     return TrustedProtocol, TrustedConnection
