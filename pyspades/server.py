86176c591fa6cf5f58e0d1f60738c18b85d38e87
duckslingers <duckslingers@gmail.com>
2012-07-05 21:43:36 -0300

    *Actually* fix issue 263

--- pyspades/server.py
+++ pyspades/server.py
@@ -517,7 +517,7 @@ class ServerConnection(BaseConnection):
                         if not map.get_solid(x, y, z):
                             return
                         pos = world_object.position
-                        if self.weapon == SPADE_TOOL and not collision_3d(
+                        if self.tool == SPADE_TOOL and not collision_3d(
                             pos.x, pos.y, pos.z, x, y, z, MAX_DIG_DISTANCE):
                             return
                         if self.on_block_destroy(x, y, z, value) == False:
