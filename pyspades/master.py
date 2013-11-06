f955a446fba66adbd042e832569f0cd49702d873
Nate Shoffner <nate.shoffner@gmail.com>
2013-11-06 10:54:00 -0500

    Updated IP getter.

--- pyspades/master.py
+++ pyspades/master.py
@@ -102,7 +102,7 @@ class MasterConnection(BaseConnection):
 
 from pyspades.web import getPage
 
-IP_GETTER = 'http://automation.whatismyip.com/n09230945.asp'
+IP_GETTER = 'http://services.buildandshoot.com/getip'
 
 def get_external_ip(interface = ''):
     return getPage(IP_GETTER, bindAddress = (interface, 0))
