8b1c1184429f4ef709b3f7ae6e4746336891560a
Way, No <noway@2ch.hk>
2015-04-13 22:25:07 +1200

    Auto venv loading

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+cd "$(dirname "$(readlink -f "$(command -v "$0")")")"
+if [ -d "venv/" ] && [ ! "x$(command -v python)" == "x$VIRTUAL_ENV/bin/python" ]; then
+	source ./venv/bin/activate
+	echo "Using virtualenv"
+fi
+
 # any compiled file whatsoever
 
 find . -type f \( \
