1ae780d90e44ec07703a9b71470afc27c4ed5158
Way, No <noway@2ch.hk>
2016-12-31 17:31:31 +1300

    setup.py from pyenet is being ran

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import distutils
 from setuptools import setup, find_packages, Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
@@ -22,6 +24,24 @@ for name in names:
     ext_modules.append(Extension(name, ['%s.pyx' % name.replace('.', '/')],
         language = 'c++', include_dirs=['pyspades'], **extra))
 
+from distutils.command.build import build as _build
+
+
+class build(_build):
+    def run(self):
+        print "** running enet"
+        distutils.core.run_setup(os.path.join(sys.path[0], "enet/setup.py"), ['build_ext', '--inplace'])
+        print "** ran enet"
+        _build.run(self)
+        # self.execute(_run_build_tables, (self.install_lib,),
+        #              msg="Build the lexing/parsing tables")
+
+pyspades_ext_modules = cythonize(ext_modules) 
+
+from pprint import pprint
+print "-- ext modules"
+print pyspades_ext_modules
+pprint(pyspades_ext_modules[0].__dict__)
 
 setup(
     name = 'pysnip',
@@ -49,5 +69,6 @@ setup(
     package_dir = {'pysnip': 'feature_server', 'pysnip.web': 'feature_server/web', 'pyspades': 'pyspades', 'pyspades.enet': 'enet'}, # some kind of find_packages?
     package_data = {"pyspades.enet": ["enet.so"], "pysnip.web": ["templates/status.html"]},
 
-    ext_modules = cythonize(ext_modules)
+    ext_modules = cythonize(ext_modules),
+    cmdclass = {'build': build},
 )
