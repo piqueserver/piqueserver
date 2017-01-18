b17eb5e63fa2b4e8a4484fbc265b8b31ff9c9200
Samuel Walladge <samuel@swalladge.id.au>
2017-01-18 12:57:43 +1030

    fix manifest removing enet.so

--- setup.py
+++ setup.py
@@ -167,7 +167,7 @@ setup(
         ],
     },
     package_dir = {PKG_NAME: 'feature_server', '%s.web' % PKG_NAME: 'feature_server/web', 'pyspades': 'pyspades', 'pyspades.enet': 'enet/pyenet'}, # some kind of find_packages?
-    package_data = {"pyspades.enet": ["enet.so"], "%s.web" % PKG_NAME: ["templates/status.html"]},
+    package_data = {"%s.web" % PKG_NAME: ["templates/status.html"]},
     include_package_data=True,
 
     ext_modules = ext_modules,
