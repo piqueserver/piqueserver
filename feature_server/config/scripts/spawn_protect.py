41499985acb9234297ff25f8ac0b0c68f28f0b4e
kmsiapps <kmsiapps@gmail.com>
2017-01-22 16:21:30 +0900

    Update spawn_protect.py

--- feature_server/config/scripts/spawn_protect.py
+++ feature_server/config/scripts/spawn_protect.py
@@ -26,4 +26,3 @@ def apply_script(protocol, connection, config):
                 return False
             return connection.on_hit(self, hit_amount, hit_player, type, grenade)
     return protocol, SpawnProtectConnection
-    
