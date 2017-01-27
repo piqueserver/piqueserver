a1f56e78f5dbdd71c0b1374d897f2a92a01af395
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2017-01-27 05:26:31 -0200

    Fix merge problems

---
+++ feature_server/cfg.py
@@ -0,0 +1,13 @@
+# to have global configuration variables
+# similar to http://effbot.org/pyfaq/how-do-i-share-global-variables-across-modules.htm
+
+from __future__ import absolute_import, division, print_function
+import os
+
+config = {}
+pkg_name = "piqueserver"
+config_file = 'config.json'
+
+prefix = os.environ.get('XDG_CONFIG_DIR', '~/.config')
+config_path = prefix + "/piqueserver"
+config_dir = os.path.expanduser(config_path)
