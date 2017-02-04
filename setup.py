d4e862f3289cfc344ba92ab305c698a5a0d2fe7a
notafile <notafile@gmail.com>
2017-02-04 22:07:00 +0100

    add progress bar for enet download

--- setup.py
+++ setup.py
@@ -84,7 +84,19 @@ def compile_enet():
 
     if not os.path.isfile(enet_file):
         print("Downloading enet")
-        urllib_request.urlretrieve(enet_url, enet_file)
+
+        def reporthook(blocknum, blocksize, totalsize):
+            readsofar = blocknum * blocksize
+            if totalsize > 0:
+                percent = readsofar * 1e2 / totalsize
+                s = "\r%5.1f%% %*d / %d" % (
+                    percent, len(str(totalsize)), readsofar, totalsize)
+                sys.stderr.write(s)
+                if readsofar >= totalsize: # near the end
+                    sys.stderr.write("\n")
+            else: # total size is unknown
+                sys.stderr.write("read %d\n" % (readsofar,))
+        urllib_request.urlretrieve(enet_url, enet_file, reporthook)
         print("Finished downloading enet")
 
     print("Unpacking enet")
