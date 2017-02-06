667b09be2a7efcae2f2f1dad2ab48e9773d14e67
notafile <notafile@gmail.com>
2017-02-06 01:58:50 +0100

    add attribution for snippet

--- setup.py
+++ setup.py
@@ -85,6 +85,9 @@ def compile_enet():
     if not os.path.isfile(enet_file):
         print("Downloading enet")
 
+        # begin code shamelessly stolen from StackOverflow
+        # http://stackoverflow.com/a/13895723/2730399
+        # Answer by https://stackoverflow.com/users/4279/j-f-sebastian
         def reporthook(blocknum, blocksize, totalsize):
             readsofar = blocknum * blocksize
             if totalsize > 0:
@@ -98,6 +101,7 @@ def compile_enet():
                 sys.stderr.write("read %d\n" % (readsofar,))
         urllib_request.urlretrieve(enet_url, enet_file, reporthook)
         print("Finished downloading enet")
+        # end code shamelessly stolen from SackOverflow
 
     print("Unpacking enet")
     tar = tarfile.open(enet_file)
