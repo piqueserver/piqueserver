d639acf433a734f8a6287497f31f29f4b7d2148f
Samuel Walladge <samuel@swalladge.id.au>
2017-12-19 20:45:38 +1030

    add scripts to build binary wheels

---
+++ scripts/_docker_build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# should be run in the docker container.
+# builds the binary wheels
+
+set -e -x
+
+# install pandoc
+wget https://github.com/jgm/pandoc/releases/download/2.0.5/pandoc-2.0.5-linux.tar.gz -O pandoc.tar.gz
+tar xvzf pandoc.tar.gz --strip-components 1 -C /usr/local/
+
+# code from https://github.com/pypa/python-manylinux-demo/blob/master/travis/build-wheels.sh
+# Compile wheels
+for PYBIN in /opt/python/{cp27-cp27m,cp27-cp27mu,cp34-cp34m,cp35-cp35m,cp36-cp36m}/bin; do
+    "${PYBIN}/pip" install pypandoc
+    "${PYBIN}/pip" install --only-binary Pillow -r /io/requirements.txt
+    cd /io/
+    "${PYBIN}/python" setup.py bdist_wheel -d /wheelhouse/
+    cd /
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
+# fix perms so it's possible to remove without being root
+chmod go+w -R build wheelhouse
