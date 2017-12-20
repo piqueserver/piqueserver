fdff3deac0cbb707553f714b93625aee9b100ad2
Samuel Walladge <samuel@swalladge.id.au>
2017-12-20 22:41:05 +1030

    missed a directory in the docker build script

--- scripts/_docker_build.sh
+++ scripts/_docker_build.sh
@@ -25,4 +25,4 @@ for whl in wheelhouse/*.whl; do
 done
 
 # fix perms so it's possible to remove without being root
-chmod go+w -R /io/build /io/wheelhouse
+chmod -f go+w -R /io/build /io/wheelhouse /io/piqueserver.egg-info/
