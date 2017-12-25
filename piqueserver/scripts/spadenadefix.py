48245176c7830c8c4a1e7a87451c8ed7da84cd7e
Adrian Tschira <notafile@gmail.com>
2017-12-25 12:29:48 +0100

    style fixes in spadenadefix

--- piqueserver/scripts/spadenadefix.py
+++ piqueserver/scripts/spadenadefix.py
@@ -9,15 +9,15 @@ from pyspades.constants import SPADE_TOOL
 def apply_script(protocol, connection, config):
     class SpadenadeConnection(connection):
         def on_secondary_fire_set(self, secondary):
-            self.wasSpade = (self.tool == SPADE_TOOL)
-        
+            self.was_spade = (self.tool == SPADE_TOOL)
+
         def on_grenade(self, time_left):
-            if(self.world_object.secondary_fire and self.wasSpade):
+            if(self.world_object.secondary_fire and self.was_spade):
                 self.send_chat('Spade-Grenade bug is blocked.')
-                self.wasSpade = False
+                self.was_spade = False
                 return False
             else:
-                self.wasSpade = False
+                self.was_spade = False
                 return connection.on_grenade(self, time_left)
 
     return protocol, SpadenadeConnection
