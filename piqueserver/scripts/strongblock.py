d73ad8cc58314c1432127877ea5d88b62ae4b1e7
kmsiapps <kmsiapps@gmail.com>
2017-12-26 21:37:59 +0900

    Fix rangedamage and strongblock
    for rangedamage: don't apply damage falloff for grenade (e.g. airstrike)

--- piqueserver/scripts/strongblock.py
+++ piqueserver/scripts/strongblock.py
@@ -80,7 +80,7 @@ def apply_script(protocol, connection, config):
 
         def on_block_destroy(self, x, y, z, value):
             can_destroy = connection.on_block_destroy(self, x, y, z, value)
-            if can_destroy:
+            if can_destroy != False:
                 strong_block = self.protocol.strong_blocks.pop((x, y, z), None)
                 if strong_block is not None:
                     # block is a strong block
