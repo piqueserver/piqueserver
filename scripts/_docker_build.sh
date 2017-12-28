c85be97b8fef2d2b54563c9e28470cf6dff5d1bc
Samuel Walladge <samuel@swalladge.id.au>
2017-12-28 21:47:52 +1030

    make sure intermediate leftovers are removed between builds

--- scripts/_docker_build.sh
+++ scripts/_docker_build.sh
@@ -8,15 +8,19 @@ set -e -x
 
 # code from https://github.com/pypa/python-manylinux-demo/blob/master/travis/build-wheels.sh
 # Compile wheels
+cd /io/
 for PYBIN in /opt/python/{cp27-cp27m,cp27-cp27mu,cp34-cp34m,cp35-cp35m,cp36-cp36m}/bin; do
+    # clean previous cached stuff
+    rm -rf /io/build/
+    git clean -fX -- pyspades
     "${PYBIN}/pip" install --only-binary Pillow -r /io/requirements.txt
-    cd /io/
     "${PYBIN}/python" setup.py bdist_wheel -d /wheelhouse/
-    cd /
 done
 
+cd /
+
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in /wheelhouse/*.whl; do
     auditwheel show "$whl"
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
