26e08c44e98987935a69b734a0b37de617465631
Godwhoa <gjdaniel1999@gmail.com>
2018-02-05 00:08:40 +0530

    Switch to readthedocs default theme

--- doc/conf.py
+++ doc/conf.py
@@ -33,6 +33,8 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import os
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon', # so we can use google style doc strings
@@ -95,7 +97,11 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
