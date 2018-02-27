39e5ad7510dbcde36198894931069099da55177f
jonassjoh <jonassjoh@gmail.com>
2018-02-27 16:14:19 +0100

    feat: Adds test for badmin.py

--- tests/piqueserver/test_badmin.py
+++ tests/piqueserver/test_badmin.py
@@ -3,13 +3,25 @@ test piqueserver/scripts/badmin.py
 """
 
 from twisted.trial import unittest
+from twisted.internet import reactor
 from unittest.mock import MagicMock
-from piqueserver.scripts.badmin import score_grief
+from piqueserver.scripts.badmin import *
+
+def get_mock_block():
+    b = MagicMock(), MagicMock()
+    return b
 
 class TestBAdmin(unittest.TestCase):
 
     def test_score_grief_0(self):
         connection = MagicMock()
         player = MagicMock()
+        score = score_grief(connection, player)
+        self.assertTrue(score is 0)
+
+    def test_score_grief_1(self):
+        connection = MagicMock()
+        player = MagicMock()
+        player.blocks_removed = [(reactor.seconds(), get_mock_block())]
         score = score_grief(connection, player)
         self.assertTrue(score is 0)
