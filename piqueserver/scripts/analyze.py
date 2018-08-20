a555791d962b315d5b89163376d80ecd01febb09
Godwhoa <gjdaniel1999@gmail.com>
2018-08-20 07:49:27 +0530

    Add analyze to included scripts Fix formatting a bit in analyze

--- piqueserver/scripts/analyze.py
+++ piqueserver/scripts/analyze.py
@@ -4,11 +4,9 @@ Shows a detailed analysis of a players shots
 Commands
 ^^^^^^^^
 
-* ``/analyze or /an <target>`` to show a detailed analysis of a players shots if they hit somone.
-|	hit player, distance, dT in miliseconds (Delta Time- Time since previous shot that
-|	hit someone.  Useful for detecting multiple bullet or rapid hacks), weapon, what
-|	body part it hit, and a basic counter that displays the number of hits
-|	of that type.
+* ``/analyze or /an <target>`` to show a detailed analysis of a players shots if they hit someone. 
+  Shows hit player, distance, dT in miliseconds (Delta Time- Time since previous shot that hit someone. 
+  Useful for detecting multiple bullet or rapid hacks), weapon, which body part it hit, and a basic counter that displays the number of hits of that type.
 
 * ``/analyze or /an`` to disable it
 
