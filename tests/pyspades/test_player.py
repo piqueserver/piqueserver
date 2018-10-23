477d577936fe6e0c2aca4e66fede90426b240d76
NotAFile <nota@notafile.com>
2018-10-23 02:08:23 +0200

    add team join test

--- tests/pyspades/test_player.py
+++ tests/pyspades/test_player.py
@@ -3,13 +3,27 @@ test pyspades/protocol.py
 """
 
 from twisted.trial import unittest
-from pyspades import player
+from pyspades import player, server, contained
+from pyspades.team import Team
 from unittest.mock import Mock
 
 class BaseConnectionTest(unittest.TestCase):
-    def test_dummy(self):
-        pass
-
     def test_repr(self):
         ply = player.ServerConnection(Mock(), Mock())
         repr(ply)
+
+    def test_team_join(self):
+        prot = Mock()
+        prot.team_class = Team
+        server.ServerProtocol._create_teams(prot)
+        # Some places still use the old name
+        prot.players = {}
+
+        for team in (prot.team_1, prot.team_2, prot.team_spectator):
+            ply = player.ServerConnection(prot, Mock())
+            ply.spawn = Mock()
+            ex_ply = contained.ExistingPlayer()
+            ex_ply.team = team.id
+            ply.on_new_player_recieved(ex_ply)
+
+            self.assertEqual(ply.team, team)
