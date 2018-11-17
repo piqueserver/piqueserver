05bf103fa0d9c0c7480929412a3be73ddc4c5026
Godwhoa <gjdaniel1999@gmail.com>
2018-11-18 01:11:05 +0530

    Implement and test ConfigAuthBackend

--- piqueserver/auth.py
+++ piqueserver/auth.py
@@ -1,13 +1,40 @@
+from typing import Tuple
 import abc
+from piqueserver.config import config
+from piqueserver.player import FeatureConnection
+
+Details = Tuple[str, str]  # username, password
+
+
+class AuthError(Exception):
+    pass
 
 
 class BaseAuthBackend(abc.ABC):
     @abc.abstractmethod
-    def login(self, details: Tuple):
+    def login(self, details: Details) -> str:
+        """Verifies details and returns an user_role. 
+        Raises AuthError if details are incorrect."""
         pass
+
+    @abc.abstractmethod
+    def has_permision(self, connection: FeatureConnection, action: str) -> bool:
+        pass
+
 
 class ConfigAuthBackend(BaseAuthBackend):
     """Auth backend that uses the [passwords] section of the connfig for
     authentication"""
-    def login(self, username):
-        pass
+
+    def __init__(self):
+        self.passwords = config.option('passwords', default={}).get()
+
+    def login(self, details: Details) -> str:
+        _, password = details
+        for user_type, passwords in self.passwords.items():
+            if password in passwords:
+                return user_type
+        raise AuthError
+
+    def has_permision(self, connection: FeatureConnection, action: str) -> bool:
+        return connection.admin or action in connection.rights
---
+++ tests/piqueserver/test_auth.py
@@ -0,0 +1,41 @@
+import unittest
+from piqueserver.config import config
+from piqueserver.auth import ConfigAuthBackend, AuthError
+from piqueserver.player import FeatureConnection
+from pyspades.types import AttributeSet
+
+
+class MockConnection(object):
+    admin = False
+    rights = AttributeSet()
+
+
+class TestConfigAuthBackend(unittest.TestCase):
+    def test_login_ok(self):
+        config.update_from_dict({
+            "passwords": {
+                "moderator": ["mod1", "mod2"],
+                "guard": ["guard1", "guard2"]
+            }
+        })
+        auth = ConfigAuthBackend()
+        self.assertEqual(auth.login(("", "mod2")), "moderator")
+        self.assertEqual(auth.login(("", "guard1")), "guard")
+        self.assertRaises(AuthError, lambda: auth.login(("", "mod3")))
+        self.assertRaises(AuthError, lambda: auth.login(("", "guard5")))
+
+    def test_has_permission_admin(self):
+        connection = MockConnection()
+        connection.admin = True
+        auth = ConfigAuthBackend()
+        has = auth.has_permision(connection, "ban")
+        self.assertTrue(has)
+
+    def test_has_permission_rights(self):
+        connection = MockConnection()
+        connection.rights = AttributeSet(['hban', 'kick'])
+        auth = ConfigAuthBackend()
+        for case in [("ban", False), ("hban", True), ("kick", True)]:
+            action, expected = case
+            got = auth.has_permision(connection, action)
+            self.assertEqual(got, expected)
