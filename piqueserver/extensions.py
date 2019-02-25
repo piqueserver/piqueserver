e46a98d0cbe5f9b8a16bff659568640ef78a5fcf
arnthor89 <arnthorj2@gmail.com>
2019-02-25 13:41:34 +0100

    #166 [refactor] Add new methods to extensions.py

---
+++ piqueserver/extensions.py
@@ -0,0 +1,34 @@
+
+import os
+
+
+def check_scripts(scripts):
+    '''
+    Checks if scripts were included multiple times.
+    '''
+    seen = set()
+    dups = []
+    for script in scripts:
+        if script in seen:
+            dups.append(script)
+        else:
+            seen.add(script)
+    if dups:
+        log.warn("Scripts included multiple times: {}".format(dups))
+        return False
+    return True
+
+
+def load_scripts(config):
+    '''
+    Loads all scripts from script/ folder
+    Returns a list of script modules
+    '''
+
+
+def apply_scripts(scripts, config):
+    '''
+    Applies scripts to the server
+    Returns protocol and connection class
+    '''
+
