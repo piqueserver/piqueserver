b6ba29e1a8af6143f7c09f45fd95cda2a1dedc89
arnthor89 <arnthorj2@gmail.com>
2019-02-26 11:51:54 +0100

    #166 [feat] Add test for check_game_mode

--- tests/piqueserver/test_extensions.py
+++ tests/piqueserver/test_extensions.py
@@ -10,15 +10,18 @@ from piqueserver.config import config
 from piqueserver.server import FeatureProtocol
 from piqueserver.player import FeatureConnection
 
+
 class TestExtensions(unittest.TestCase):
-    # Define scripts
+    # Define test scripts
     script_names = ['test_script_game_mode', 'test_script_regular']
     script_names_length = len(script_names)
-    # Get current directory
-    curr_dir = os.getcwd()
-    script_dir = os.path.join(curr_dir, 'piqueserver/test_scripts/')
-    # Load modules
-    script_objects = extensions.load_scripts(script_names, script_dir, 'testscript')
+
+    def get_test_scripts(self):
+        # Get test scripts directory
+        script_dir = os.path.join(os.path.dirname(__file__), "test_scripts/")
+        # Load modules
+        return extensions.load_scripts(
+            self.script_names, script_dir, 'testscript')
 
     def test_checkscripts(self):
         scripts = []
@@ -37,20 +40,29 @@ class TestExtensions(unittest.TestCase):
         self.assertFalse(extensions.check_scripts(scripts))
 
     def test_load_scripts(self):
-        self.assertTrue(len(self.script_objects) == self.script_names_length)
+        script_objects = self.get_test_scripts()
+        self.assertTrue(len(script_objects) == self.script_names_length)
+
         # apply_script method should exist in all modules
-        for i in range(0,len(self.script_objects)):
-            self.assertTrue("apply_script" in dir(self.script_objects[i]))
+        for script in script_objects[:]:
+            self.assertTrue("apply_script" in dir(script))
 
     def test_apply_scripts(self):
+        script_objects = self.get_test_scripts()
         protocol_class = FeatureProtocol
         connection_class = FeatureConnection
 
         self.assertNotEqual(protocol_class.game_mode, "testing")
         self.assertTrue(connection_class.killing)
 
-        (protocol_class, connection_class) = extensions.apply_scripts(self.script_objects, config, protocol_class, connection_class)
+        (protocol_class, connection_class) = extensions.apply_scripts(
+            script_objects, config, protocol_class, connection_class)
 
         self.assertEqual(protocol_class.game_mode, "testing")
         self.assertFalse(connection_class.killing)
 
+    def test_check_game_mode(self):
+        self.assertFalse(extensions.check_game_mode('ctf'))
+        self.assertFalse(extensions.check_game_mode('tc'))
+        self.assertTrue(extensions.check_game_mode('tcc'))
+        self.assertTrue(extensions.check_game_mode('md'))
