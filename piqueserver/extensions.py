a5dd409a3fa7f94d7922f7e3fb4042a685ea472e
arnthor89 <arnthorj2@gmail.com>
2019-02-26 13:25:32 +0100

    #166 [refactor] Using string interpolation and ran autopep8

--- piqueserver/extensions.py
+++ piqueserver/extensions.py
@@ -7,6 +7,7 @@ from twisted.logger import Logger
 
 log = Logger()
 
+
 def check_scripts(script_names):
     '''
     Ensures that there are no duplicate scripts in scripts
@@ -25,11 +26,13 @@ def check_scripts(script_names):
         return False
     return True
 
+
 def check_game_mode(game_mode_name):
     '''
     Check if the game_mode is not a default one.
     '''
     return game_mode_name not in ('ctf', 'tc')
+
 
 def load_scripts(script_names, script_dir, script_type):
     '''
@@ -46,8 +49,8 @@ def load_scripts(script_names, script_dir, script_type):
             # this finds and loads scripts directly from the script dir
             # no need for messing with sys.path
             f, filename, desc = imp.find_module(script, [script_dir])
-            module = imp.load_module(
-                'piqueserver_' + script_type + '_namespace_' + script, f, filename, desc)
+            module = imp.load_module('piqueserver_{}_namespace_'.format(
+                script_type) + script, f, filename, desc)
             script_objects.append(module)
         except ImportError as e:
             # warning: this also catches import errors from inside the script
@@ -56,16 +59,20 @@ def load_scripts(script_names, script_dir, script_type):
                 module = importlib.import_module(script)
                 script_objects.append(module)
             except ImportError as e:
-                log.error("(" + script_type + "'{}' not found: {!r})".format(script, e))
+                log.error(
+                    "('{} {}' not found: {!r})".format(
+                        script_type, script, e))
                 script_names.remove(script)
 
     return script_objects
+
 
 def load_scripts_regular_extension(script_names, script_dir):
     '''
     Wrapper that load scripts in the case of regular extension scripts
     '''
     return load_scripts(script_names, script_dir, 'script')
+
 
 def load_script_game_mode(script_name, script_dir):
     '''
