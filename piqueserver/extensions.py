a0424cef3b5492731c73f6c85c18baeeaf9b46f1
Alberto Dubbini <alberto.dubbini@mail.polimi.it>
2019-02-26 13:52:01 +0100

    #166 [doc] Add documentation for extension.py

--- piqueserver/extensions.py
+++ piqueserver/extensions.py
@@ -8,10 +8,15 @@ from twisted.logger import Logger
 log = Logger()
 
 def check_scripts(script_names):
-    '''
-    Ensures that there are no duplicate scripts in scripts
-    :param scripts: list of scripts
-    :return: True if there is no duplicate scripts, False otherwise
+    '''Validation for a list of regular extension scripts.
+
+    Check there are no duplicate scripts in the list of scripts names that is passed in.
+
+    Args:
+        script_names: The list of scripts names to be checked
+
+    Return:
+        bool: True if there are no duplicate scripts, False otherwise
     '''
     seen = set()
     dups = []
@@ -26,18 +31,32 @@ def check_scripts(script_names):
     return True
 
 def check_game_mode(game_mode_name):
-    '''
-    Check if the game_mode is not a default one.
+    '''Validation for a game mode script.
+
+    Check if the game_mode is not a default one ('ctf' or 'tc').
+
+    Args:
+        game_mode_name: The game mode name to be checked
+
+    Return:
+        bool: True if the game mode is not a default one, False otherwise
     '''
     return game_mode_name not in ('ctf', 'tc')
 
 def load_scripts(script_names, script_dir, script_type):
-    '''
-    Loads all scripts from the script_dir folder
-    :param script_names: A list of script names
-    :param script_dir: Path to scripts directory
-    :param script_type: A string; "script" for regular scripts and "gamemode" for game_mode script
-    :return: A list of script modules
+    '''Load script as module.
+
+    Loads all scripts, matching the script_names, from the script_dir folder. The function also requires the
+    specification of which type of scripts are passed in (e.g. "script", "gamemode", "testscript"). This is
+    necessary for naming the namespace and error handling.
+
+    Args:
+        script_names: The list of script names to be loaded
+        script_dir: The path to the corresponding scripts directory
+        script_type: The script type ("script" for regular scripts and "gamemode" for game_mode script)
+
+    Return:
+        [module]: The list of module objects containing the loaded scripts
     '''
     script_objects = []
 
@@ -62,14 +81,33 @@ def load_scripts(script_names, script_dir, script_type):
     return script_objects
 
 def load_scripts_regular_extension(script_names, script_dir):
-    '''
-    Wrapper that load scripts in the case of regular extension scripts
+    ''' Wrapper for load function
+
+    It loads scripts in the case of regular extension scripts.
+
+    Args:
+        script_names: The list of script names to be loaded
+        script_dir: The path to the corresponding scripts directory
+
+    Return:
+        [module]: The list of module objects containing the loaded scripts
+
     '''
     return load_scripts(script_names, script_dir, 'script')
 
 def load_script_game_mode(script_name, script_dir):
-    '''
-    Wrapper that load scripts in the case of the game mode script
+    ''' Wrapper for load function
+
+    It loads scripts in the case of game mode scripts. Prior to this, it checks if the game mode is not a
+    default one (if it's a default no scripts should be loaded)
+
+    Args:
+        script_names: The list of script names to be loaded
+        script_dir: The path to the corresponding scripts directory
+
+    Return:
+        [module]: The list of module objects containing the uploaded scripts
+
     '''
     if check_game_mode(script_name):
         return load_scripts(script_name, script_dir, 'gamemode')
@@ -77,13 +115,22 @@ def load_script_game_mode(script_name, script_dir):
 
 
 def apply_scripts(scripts, config, protocol_class, connection_class):
-    '''
-    Applies scripts to the specified protocol and connection class instances
-    :param scripts: List of scripts modules to apply
-    :param config: Config object which holds a dict
-    :param protocol_class: The protocol class instance to update
-    :param connection_class: The connection class instance to update
-    :return: The updated protocol and connection class instances
+    ''' Application of scripts modules
+
+    It applies the script modules to the specified protocol and connection class instances, in order to build
+    the resulting classes with all the additional features that the scripts implement (more information
+    about how scripts are implemented and used can be found in the project documentation
+    https://piqueserver.readthedocs.io/en/latest/architecture.html#extension-scripts).
+
+    Args:
+        scripts: List of scripts modules to apply
+        config: It holds the dict of sections and options; this is required by the scripts
+         logic
+        protocol_class: The protocol class instance to update
+        connection_class: The connection class instance to update
+
+    Return:
+        (FeatureProtocol, FeatureConnection): The updated protocol and connection class instances
     '''
 
     for script in scripts:
