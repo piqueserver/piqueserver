33ad8d62ae4c4495428b5ada43b24b6cd74b7f9a
NotAFile <notafile@gmail.com>
2020-04-20 19:53:29 +0200

    fix type annotations for async utils

--- piqueserver/utils/_async.py
+++ piqueserver/utils/_async.py
@@ -1,11 +1,12 @@
 import asyncio
 from asyncio import Future
 from twisted.internet.defer import ensureDeferred, Deferred
+from typing import Awaitable
 
 
-def as_future(d: Deferred):
+def as_future(d: Deferred) -> Future:
     return d.asFuture(asyncio.get_event_loop())
 
 
-def as_deferred(f: Future):
+def as_deferred(f: Awaitable) -> Deferred:
     return Deferred.fromFuture(asyncio.ensure_future(f))
