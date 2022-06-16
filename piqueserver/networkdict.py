e004b50e2896a8b263e901fd3a9052555b1529a4
DryByte <93609026+DryByte@users.noreply.github.com>
2022-06-16 08:36:16 -0300

    fix wrong bans being deleted

--- piqueserver/networkdict.py
+++ piqueserver/networkdict.py
@@ -70,10 +70,10 @@ class NetworkDict:
 
     def __delitem__(self, key):
         ip = ip_network(str(key), strict=False)
-        self.networks.popitem(ip)
+        self.networks.pop(ip)
 
     def pop(self, *arg, **kw):
-        network, value = self.networks.popitem(*arg, **kw)
+        network, value = self.networks.pop(*arg, **kw)
         return get_cidr(network), value
 
     def iteritems(self):
