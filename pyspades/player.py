a47ec17e02bd8d7615189dc14160675b8684e25d
utf <utf16+github@riseup.net>
2024-02-11 17:12:56 +0100

    fix(player): drop grenades with invalid length

--- pyspades/player.py
+++ pyspades/player.py
@@ -418,7 +418,10 @@ class ServerConnection(BaseConnection):
         if contained.value > 3.0:
             contained.value = 3.0
         velocity = Vertex3(*contained.velocity) - self.world_object.velocity
-        if velocity.length() > 2.0:  # cap at tested maximum
+        length = velocity.length()
+        if check_nan(length):
+            return
+        if length > 2.0:  # cap at tested maximum
             velocity = velocity.normal() * 2.0
         velocity += self.world_object.velocity
         if self.on_grenade(contained.value) == False:
