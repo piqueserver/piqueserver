f86f908dcddb949296b78ad4cb8a3d3adfe43c80
utf <utf16+github@riseup.net>
2024-05-18 15:37:12 +0200

    refactor: modernize package, move to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,162 +1,91 @@
-#!/usr/bin/env python3
-
-import sys
 import os
-from distutils.core import run_setup
-# build_ext is subclassed, so we import it with a _ to avoid a collision
-from distutils.command.build_ext import build_ext as _build_ext
+from typing import Dict, List, NamedTuple
 from setuptools import setup, Extension, find_packages
+from Cython.Build import cythonize
+from Cython.Compiler import Options
 
+class Descriptor(NamedTuple):
+    extension_name: str
+    sources: List[str]
+    compile_flags: List[str] = []
+    link_flags: List[str] = []
+    macros: Dict[str, str] = {}
 
-PKG_NAME = "piqueserver"
+use_static = (os.environ.get('STDCPP_STATIC') == '1')
+use_asan = (os.environ.get('USE_ASAN') == '1')
+use_ubsan = (os.environ.get('USE_UBSAN') == '1')
+use_linetrace = (os.environ.get('CYTHON_TRACE') == '1')
 
-extra_args = sys.argv[2:]
+compile_flags: List[str] = []
+link_flags: List[str] = []
+macros: Dict[str, str] = {}
 
-with open('README.rst') as f:
-    long_description = f.read()
+if use_static:
+    link_flags += ['-static-libstdc++', '-static-libgcc']
 
-# load version info from the piqueserver module manually
-here = os.path.abspath(os.path.dirname(__file__))
-version = {}
-with open(os.path.join(here, 'piqueserver/version.py')) as f:
-    exec(f.read(), version)
+# Compile the server with AddressSanitizer
+if use_asan:
+    link_flags += ['-lasan']
+    compile_flags += ['-fsanitize=address']
 
-ext_names = [
-    'pyspades.vxl',
-    'pyspades.bytes',
-    'pyspades.packet',
-    'pyspades.contained',
-    'pyspades.common',
-    'pyspades.world',
-    'pyspades.loaders',
-    'pyspades.mapmaker'
+# Compile the server with UndefinedBehaviourSanitizer
+if use_ubsan:
+    link_flags += ['-lubsan']
+    compile_flags += ['-fsanitize=undefined']
+
+if use_linetrace:
+    macros['CYTHON_TRACE'] = '1'
+    Options.generate_cleanup_code = True
+
+extension_descriptors = [
+    Descriptor('pyspades.world',
+               sources=['pyspades/world.pyx'],
+               compile_flags=['-std=c++11']),
+
+    Descriptor('pyspades.mapmaker',
+               sources=['pyspades/mapmaker.pyx'],
+               compile_flags=['-std=c++11']),
+
+    Descriptor('pyspades.bytes',
+               sources=['pyspades/bytes.pyx']),
+
+    Descriptor('pyspades.common',
+               sources=['pyspades/common.pyx']),
+
+    Descriptor('pyspades.contained',
+               sources=['pyspades/contained.pyx']),
+
+    Descriptor('pyspades.loaders',
+               sources=['pyspades/loaders.pyx']),
+
+    Descriptor('pyspades.packet',
+               sources=['pyspades/packet.pyx']),
+
+    Descriptor('pyspades.vxl',
+               sources=['pyspades/vxl.pyx']),
 ]
 
-static = os.environ.get('STDCPP_STATIC') == "1"
+extensions: List[Extension] = []
+for descriptor in extension_descriptors:
+    extension = Extension(
+        name=descriptor.extension_name,
+        sources=descriptor.sources,
+        language='c++',
+        include_dirs=['pyspades/'],
+        define_macros=list({**macros, **descriptor.macros}.items()),
+        extra_link_args=[*link_flags, *descriptor.link_flags],
+        extra_compile_args=[*compile_flags, *descriptor.compile_flags],
+    )
 
-if static:
-    print("Linking the build statically.")
-
-linetrace = os.environ.get('CYTHON_TRACE') == '1'
-
-# Compile the server with support for
-# AddressSanitizer/UndefinedBehaviourSanitizer
-USE_ASAN = os.environ.get('USE_ASAN') == '1'
-USE_UBSAN = os.environ.get('USE_UBSAN') == '1'
-
-ext_modules = []
-
-for name in ext_names:
-    extra = {
-        "define_macros": [],
-        "extra_link_args": [],
-        "extra_compile_args": [],
-    }  # type: dict
-
-    if static:
-        extra['extra_link_args'].extend(
-            ['-static-libstdc++', '-static-libgcc'])
-
-    if USE_ASAN:
-        extra["extra_link_args"].append("-lasan")
-        extra["extra_compile_args"].append("-fsanitize=address")
-
-    if USE_UBSAN:
-        extra["extra_link_args"].append("-lubsan")
-        extra["extra_compile_args"].append("-fsanitize=undefined")
-
-    if name in ['pyspades.vxl', 'pyspades.world', 'pyspades.mapmaker']:
-        extra["extra_compile_args"].append('-std=c++11')
-
-    if linetrace:
-        extra['define_macros'].append(('CYTHON_TRACE', '1'))
-
-    ext_modules.append(Extension(name, ['./%s.pyx' % name.replace('.', '/')],
-                                 language='c++', include_dirs=['./pyspades'],
-                                 **extra))
-
-
-class build_ext(_build_ext):
-
-    def run(self):
-
-        from Cython.Build import cythonize
-
-        if USE_ASAN:
-            from Cython.Compiler import Options
-            # make asan/valgrind's memory leak results better
-            Options.generate_cleanup_code = True
-
-        compiler_directives = {'language_level': 3, 'embedsignature': True}
-        if linetrace:
-            compiler_directives['linetrace'] = True
-
-        self.extensions = cythonize(self.extensions, compiler_directives=compiler_directives)
-
-        _build_ext.run(self)
-
-        run_setup(os.path.join(os.getcwd(), "setup.py"),
-                  ['build_py'] + extra_args)
-
+    extensions += [extension]
 
 setup(
-    name=PKG_NAME,
-    packages=find_packages(exclude=("tests", "tests.*")),
-    version=version['__version__'],
-    description='Open-Source server implementation for Ace of Spades ',
-    author=('Originally MatPow2 and PySnip contributors,'
-            'now, StackOverflow and piqueserver authors'),
-    author_email='nate.shoffner@gmail.com',
-    maintainer='noway421',
-    maintainer_email='noway@2ch.hk',
-    license='GNU General Public License v3',
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/piqueserver/piqueserver",
-    keywords=['ace of spades', 'aos', 'server',
-              'pyspades', 'pysnip', 'piqueserver'],
-    python_requires=">=3.7.0",
-    classifiers=[
-        'Intended Audience :: System Administrators',
-        'Development Status :: 5 - Production/Stable',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Unix',
-        'Operating System :: Microsoft :: Windows',
-        'Environment :: Console',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python',
-        'Programming Language :: Cython',
-        'Programming Language :: Python :: 3',
-        'Framework :: Twisted',
-        'Topic :: Games/Entertainment',
-        'Topic :: Games/Entertainment :: First Person Shooters',
-    ],
-    platforms="Darwin, Unix, Win32",
-
-    setup_requires=['Cython>=0.27,<1'],
-    install_requires=[
-        'pypiwin32;platform_system=="Windows"',
-        'Cython>=0.27,<1',
-        'Twisted[tls]',
-        'Jinja2>=2,<4',
-        'Pillow>=5.1.0,<11',
-        'aiohttp>=3.3.0,<3.8.0',
-        'pyenet',
-        'toml',
-        'packaging>=19.0'
-    ],
-    extras_require={
-        'from': ['geoip2>=2.9,<5.0'],
-        'ssh': ['Twisted[tls,conch]'],
-    },
-    entry_points={
-        'console_scripts': [
-            '%s=%s.run:main' % (PKG_NAME, PKG_NAME)
-        ],
-    },
-    package_data={"%s.web" % PKG_NAME: ["templates/status.html"]},
-    include_package_data=True,
-
-    ext_modules=ext_modules,
-    cmdclass={'build_ext': build_ext},
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            'language_level': 3,
+            'embedsignature': True,
+        }
+    ),
 )
