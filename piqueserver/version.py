815ea56c7df7a4ed4aefc0d20ed8321004380d44
utf <utf16+github@riseup.net>
2024-05-18 15:37:12 +0200

    refactor: modernize package, move to pyproject.toml

--- piqueserver/version.py
+++ piqueserver/version.py
@@ -1,2 +1,7 @@
-__version__ = '1.2.0'
-__version_info__ = (1, 2, 0)
+# this is a wrapper around piqueserver/_version.py, provided by setuptools-scm
+# pyright: reportMissingImports=false
+from piqueserver._version import (__version__ as VERSION,
+                                  __version_tuple__ as VERSION_TUPLE)
+
+__version__ = VERSION
+__version_info__ = VERSION_TUPLE
